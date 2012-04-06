eb851ddb32264b13a0487e7f81e1d1df9cb9cc6c
Noah Kantrowitz <noah@coderanger.net>
2012-04-05 23:31:29 -0700

    DRY up some more.

--- raven/spec/raven/raven_spec.rb
+++ raven/spec/raven/raven_spec.rb
@@ -3,11 +3,7 @@ require 'raven'
 
 describe 'A raven client' do
 
-  context 'being initialized with a DSN string' do
-    before do
-      @client = Raven::Client.new('http://12345:67890@sentry.localdomain/sentry/42')
-    end
-
+  shared_examples 'a configured client' do
     it 'should have a server' do
       @client.server.should == 'http://sentry.localdomain/sentry'
     end
@@ -23,6 +19,14 @@ describe 'A raven client' do
     it 'should have a project ID' do
       @client.project_id.should == '42'
     end
+  end
+
+  context 'being initialized with a DSN string' do
+    before do
+      @client = Raven::Client.new('http://12345:67890@sentry.localdomain/sentry/42')
+    end
+
+    it_should_behave_like 'a configured client'
   end
 
   context 'being initialized with a DSN option' do
@@ -30,21 +34,7 @@ describe 'A raven client' do
       @client = Raven::Client.new(:dsn => 'http://12345:67890@sentry.localdomain/sentry/42')
     end
 
-    it 'should have a server' do
-      @client.server.should == 'http://sentry.localdomain/sentry'
-    end
-
-    it 'should have a public key' do
-      @client.public_key.should == '12345'
-    end
-
-    it 'should have a secret key' do
-      @client.secret_key.should == '67890'
-    end
-
-    it 'should have a project ID' do
-      @client.project_id.should == '42'
-    end
+    it_should_behave_like 'a configured client'
   end
 
   context 'being initialized with options' do
@@ -52,22 +42,7 @@ describe 'A raven client' do
       @client = Raven::Client.new(:server => 'http://sentry.localdomain/sentry', :public_key => '12345', :secret_key => '67890', :project_id => '42')
     end
 
-    it 'should have a server' do
-      @client.server.should == 'http://sentry.localdomain/sentry'
-    end
-
-    it 'should have a public key' do
-      @client.public_key.should == '12345'
-    end
-
-    it 'should have a secret key' do
-      @client.secret_key.should == '67890'
-    end
-
-    it 'should have a project ID' do
-      @client.project_id.should == '42'
-    end
+    it_should_behave_like 'a configured client'
   end
-
 
 end
