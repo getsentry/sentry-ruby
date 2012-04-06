f7bbfc31c617c35b1feab50b9eee138808889b45
Noah Kantrowitz <noah@coderanger.net>
2012-04-05 23:25:24 -0700

    MOAR DOTS.

--- raven/spec/raven/raven_spec.rb
+++ raven/spec/raven/raven_spec.rb
@@ -47,4 +47,27 @@ describe 'A raven client' do
     end
   end
 
+  context 'being initialized with options' do
+    before do
+      @client = Raven::Client.new(:server => 'http://sentry.localdomain/sentry', :public_key => '12345', :secret_key => '67890', :project_id => '42')
+    end
+
+    it 'should have a server' do
+      @client.server.should == 'http://sentry.localdomain/sentry'
+    end
+
+    it 'should have a public key' do
+      @client.public_key.should == '12345'
+    end
+
+    it 'should have a secret key' do
+      @client.secret_key.should == '67890'
+    end
+
+    it 'should have a project ID' do
+      @client.project_id.should == '42'
+    end
+  end
+
+
 end
