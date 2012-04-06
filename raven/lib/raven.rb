270a1a5cdbc908e287c88c5e91b5057773c6b124
Noah Kantrowitz <noah@coderanger.net>
2012-04-05 23:15:36 -0700

    Basic DSN parser.

--- raven/lib/raven.rb
+++ raven/lib/raven.rb
@@ -1,5 +1,33 @@
+require 'uri'
+
 module Raven
+
   class Client
-    attr_reader :server
+
+    attr_reader :server, :public_key, :secret_key, :project_id
+
+    def initialize(dsn, options={})
+      if options.empty? && dsn.is_a?(Hash)
+        dsn, options = nil, dsn
+      end
+      dsn ||= options[:dsn]
+      dsn ||= ENV['SENTRY_DSN']
+      if dsn && !dsn.empty?
+        uri = URI::parse(dsn)
+        uri_path = uri.path.split('/')
+        options[:project_id] = uri_path.pop
+        options[:server] = "#{uri.scheme}://#{uri.host}"
+        options[:server] << ":#{uri.port}" unless uri.port == {"http"=>80,"https"=>443}[uri.scheme]
+        options[:server] << uri_path.join('/')
+        options[:public_key] = uri.user
+        options[:secret_key] = uri.password
+      end
+      @server = options[:server]
+      @public_key = options[:public_key]
+      @secret_key = options[:secret_key]
+      @project_id = options[:project_id]
+    end
+
   end
+
 end
--- raven/spec/raven/raven_spec.rb
+++ raven/spec/raven/raven_spec.rb
@@ -1,1 +1,50 @@
-require File::expand_path("../../spec_helper", __FILE__)
+require 'raven'
+require File::expand_path('../../spec_helper', __FILE__)
+
+describe 'A raven client' do
+
+  describe 'being initialized with a DSN string' do
+    before do
+      @client = Raven::Client.new('http://12345:67890@sentry.localdomain/sentry/42')
+    end
+
+    it 'should have a server' do
+      @client.server.should == 'http://sentry.localdomain/sentry'
+    end
+
+    it 'should have a public key' do
+      @client.public_key.should == '12345'
+    end
+
+    it 'should have a secret key' do
+      @client.secret_key.should == '67890'
+    end
+
+    it 'should have a project ID' do
+      @client.project_id.should == '42'
+    end
+  end
+
+  describe 'being initialized with a DSN option' do
+    before do
+      @client = Raven::Client.new(:dsn => 'http://12345:67890@sentry.localdomain/sentry/42')
+    end
+
+    it 'should have a server' do
+      @client.server.should == 'http://sentry.localdomain/sentry'
+    end
+
+    it 'should have a public key' do
+      @client.public_key.should == '12345'
+    end
+
+    it 'should have a secret key' do
+      @client.secret_key.should == '67890'
+    end
+
+    it 'should have a project ID' do
+      @client.project_id.should == '42'
+    end
+  end
+
+end
