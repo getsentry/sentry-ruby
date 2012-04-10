afbede82106e315cd3c1f97dc4842b8fba18bb91
Noah Kantrowitz <noah@coderanger.net>
2012-04-09 22:17:20 -0700

    Add a test function for making an exception.

--- raven/lib/raven.rb
+++ raven/lib/raven.rb
@@ -5,4 +5,9 @@ require 'raven/interfaces/exception'
 require 'raven/interfaces/stack_trace'
 
 module Raven
+  def self.e
+    raise Error.new('Test error')
+  rescue Error => exc
+    exc
+  end
 end
