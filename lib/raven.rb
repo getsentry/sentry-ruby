642c0442b37e6e6a2509a0cae0adde6d0e0629ea
Noah Kantrowitz <noah@coderanger.net>
2012-04-15 23:32:28 -0700

    More Rails-y configuration framework.

--- lib/raven.rb
+++ lib/raven.rb
@@ -1,3 +1,5 @@
+require 'raven/version'
+require 'raven/configuration'
 require 'raven/client'
 require 'raven/event'
 require 'raven/interfaces/message'
@@ -5,9 +7,42 @@ require 'raven/interfaces/exception'
 require 'raven/interfaces/stack_trace'
 
 module Raven
-  def self.e
-    raise Error.new('Test error')
-  rescue Error => exc
-    exc
+  class << self
+    # The client object is responsible for delivering formatted data to the Sentry server.
+    # Must respond to #send. See Raven::Client.
+    attr_accessor :client
+
+    # A Raven configuration object. Must act like a hash and return sensible
+    # values for all Raven configuration options. See Raven::Configuration.
+    attr_writer :configuration
+
+    def logger
+      @logger ||= Logger.new
+    end
+
+    # Tell the log that the client is good to go
+    def report_ready
+      self.logger.info "Raven #{VERSION} ready to catch errors"
+    end
+
+    # The configuration object.
+    # @see Raven.configure
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    # Call this method to modify defaults in your initializers.
+    #
+    # @example
+    #   Raven.configure do |config|
+    #     config.server = 'http://...'
+    #   end
+    def configure(silent = false)
+      yield(configuration)
+      self.sender = Sender.new(configuration)
+      report_ready unless silent
+      self.sender
+    end
+
   end
 end
