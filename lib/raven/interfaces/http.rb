1864d14fd130ea3be371f8e3071ce506dcba9ee9
Noah Kantrowitz <noah@coderanger.net>
2012-04-17 22:57:30 -0700

    Remove example data.

--- lib/raven/interfaces/http.rb
+++ lib/raven/interfaces/http.rb
@@ -1,20 +1,3 @@
-# {
-#     "url": "http://absolute.uri/foo",
-#     "method": "POST',
-#     "data": {
-#         "foo": "bar"
-#     },
-#     "query_string": "hello=world",
-#     "cookies": "foo=bar",
-#     "headers": {
-#         "Content-Type": "text/html"
-#     },
-#     "env": {
-#         "REMOTE_ADDR": "192.168.0.1"
-#     }
-
-# }
-
 require 'rack'
 require 'raven/interfaces'
 
