b31d7f082c17825f6cece86fbd686c230c7f474b
David Cramer <dcramer@gmail.com>
2012-07-10 19:21:22 +0200

    Exceptional seems to know Rails better than we do, so lets assume theres some unknown magic where we need ./init.rb

--- rails/init.rb
+++ rails/init.rb
@@ -1,10 +1,1 @@
-require 'raven'
-
-begin
-  Raven.logger.info("Loading Raven #{Raven::VERSION} for #{Rails::VERSION::STRING}")      
-  Rails.configuration.middleware.use "Raven::Rack"
-rescue => e
-  STDERR.puts "There was a problem configuring Raven: #{e.message}"
-  Raven.logger.error(e.message)
-  Raven.logger.error(e.backtrace)
-end
+require File.join(File.dirname(__FILE__) , '../init.rb')
