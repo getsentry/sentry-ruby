3daccdec20098cfbd2b0024b53726c20107b1155
Vincent Brendel <vincent.brendel@gmail.com>
2012-08-10 12:28:34 +1000

    Qualified class name

--- lib/raven/rails/middleware/debug_exceptions_catcher.rb
+++ lib/raven/rails/middleware/debug_exceptions_catcher.rb
@@ -7,7 +7,7 @@ module Raven
         end
 
         def render_exception_with_raven(env, exception)
-          if exception.class.name != 'ActionController::RoutingError' and exception.class.name != 'RecordNotFound'
+          if exception.class.name != 'ActionController::RoutingError' and exception.class.name != 'ActiveRecord::RecordNotFound'
             evt = Raven::Event.capture_rack_exception(exception, env)
             Raven.send(evt) if evt
           end
