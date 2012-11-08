87cfb5d15ca87e245139adce4be4aa871f71c263
John Firebaugh <john.firebaugh@gmail.com>
2012-11-07 16:38:31 -0800

    Refactor how Rack exceptions are captured
    The interface is Raven::Rack.capture_exception. The implementation
    uses the new yield behavior of Raven.capture_exception to avoid
    needing a special-case capture_rack_exception on Event or to
    manually send the event.
    
    For parallelism, define Raven::Rack.capture_message as well. See
    the next commit for a use case.

--- lib/raven/event.rb
+++ lib/raven/event.rb
@@ -150,15 +150,6 @@ module Raven
       end
     end
 
-    def self.capture_rack_exception(exc, rack_env, options={}, &block)
-      capture_exception(exc, options) do |evt|
-        evt.interface :http do |int|
-          int.from_rack(rack_env)
-        end
-        block.call(evt) if block
-      end
-    end
-
     def self.capture_message(message, options={})
       new(options) do |evt|
         evt.message = message
