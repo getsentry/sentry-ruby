f28e2354fa7a6a85ea012b94dc77fcb28857992f
Aaron Russell <aaron@gc4.co.uk>
2012-11-11 17:13:19 +0000

    Rails 2.3 legacy application support. Override ActionController::Base#rescue_action methods.

---
+++ lib/raven/rails/action_controller_catcher.rb
@@ -0,0 +1,26 @@
+module Raven
+  module Rails
+    module ActionControllerCatcher
+
+      def self.included(base)
+        base.send(:alias_method, :rescue_action_in_public_without_raven, :rescue_action_in_public)
+        base.send(:alias_method, :rescue_action_in_public, :rescue_action_in_public_with_raven)
+        base.send(:alias_method, :rescue_action_locally_without_raven, :rescue_action_locally)
+        base.send(:alias_method, :rescue_action_locally, :rescue_action_locally_with_raven)
+      end
+
+      private
+
+      def rescue_action_in_public_with_raven(exception)
+        Raven.capture_exception(exception)
+        rescue_action_in_public_without_raven(exception)
+      end
+
+      def rescue_action_locally_with_raven(exception)
+        Raven.capture_exception(exception)
+        rescue_action_locally_without_raven(exception)
+      end
+
+    end
+  end
+end
