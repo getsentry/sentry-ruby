98c4f37a8ce34135eed21a2dcabfe91b44db3a44
Andrew <chefandrew@seomoz.org>
2013-03-15 15:46:35 -0700

    Change Sentry 403 error to a logged event instead

--- spec/raven/integration_spec.rb
+++ spec/raven/integration_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 require 'raven'
+require 'raven/error'
+require 'logger'
 
 describe "Integration tests" do
 
@@ -22,4 +24,23 @@ describe "Integration tests" do
 
   end
 
+  example "hitting quota limit shouldn't swallow exception" do
+
+    stubs = Faraday::Adapter::Test::Stubs.new do |stub|
+      stub.post('/api/store') { [403, {}, 'Creation of this event was blocked'] }
+    end
+
+    Raven.configure do |config|
+      config.server = 'http://12345:67890@sentry.localdomain/sentry/42'
+      config.environments = [ "test" ]
+      config.current_environment = "test"
+      config.http_adapter = [ :test, stubs ]
+    end
+
+    Raven.logger.should_receive(:warn).exactly(1).times
+    expect { Raven.capture_exception(build_exception) }.not_to raise_error(Raven::Error)
+
+    stubs.verify_stubbed_calls
+
+  end
 end
