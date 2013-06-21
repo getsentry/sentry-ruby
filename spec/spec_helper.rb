acfed847709436abfee60a832829d27bb6f13eb3
Matt Button <matthew@sigswitch.com>
2013-06-21 15:56:12 +0100

    Adding tests for rake support, refs #111

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,40 @@ def build_exception()
     return exception
   end
 end
+
+def run_rake_task(task)
+  cwd = File.dirname(__FILE__)
+
+  rakefile = File.join(cwd, 'support', 'Rakefile')
+  tmp_dir  = File.join(cwd, 'tmp')
+
+  FileUtils.mkdir_p(tmp_dir)
+  FileUtils.cp rakefile, tmp_dir
+
+  cmd = Dir.chdir(tmp_dir) do
+    run("bundle exec rake #{task}")
+  end
+
+  FileUtils.rm_rf(tmp_dir)
+
+  cmd
+end
+
+def run(command)
+  stderr_file = Tempfile.new('rspec')
+  stderr_file.close
+
+  cmd_stdout = nil
+
+  mode = {:external_encoding=>"UTF-8"}
+
+  IO.popen("#{command} 2> #{stderr_file.path}", mode) do |io|
+    cmd_stdout = io.read
+  end
+
+  {
+    exit_status: $?.exitstatus,
+    stderr: IO.read(stderr_file.path),
+    stdout: cmd_stdout
+  }
+end
