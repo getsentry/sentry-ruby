d7c993596918c0457edd87dc1a2d7194a5b250e8
Teodor Pripoae <teodor.pripoae@gmail.com>
2013-07-20 18:21:22 +0300

    Tasks must depend on :environment (refs #115)

--- lib/raven/tasks.rb
+++ lib/raven/tasks.rb
@@ -4,7 +4,7 @@ require 'raven/cli'
 
 namespace :raven do
   desc "Send a test event to the remote Sentry server"
-  task :test, [:dsn] do |t, args|
+  task :test, [:dsn] => :environment do |t, args|
     Raven::CLI::test(args.dsn)
   end
 end
