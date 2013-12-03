f51b027148ea4f7d55b13e432ca4586a85674387
David Cramer <dcramer@gmail.com>
2013-12-03 02:14:37 -0800

    Correct accessor pattern on Context.user

--- lib/raven/context.rb
+++ lib/raven/context.rb
@@ -3,14 +3,14 @@ module Raven
     def self.current
       Thread.current[:sentry_context] ||= new
     end
-    
+
     def self.clear!
       Thread.current[:sentry_context] = nil
     end
-    
-    attr_reader :extra, :tags, :user
-    attr_accessor :rack_env
-    
+
+    attr_reader :extra, :tags
+    attr_accessor :rack_env, :user
+
     def initialize
       @extra = {}
       @tags = {}
