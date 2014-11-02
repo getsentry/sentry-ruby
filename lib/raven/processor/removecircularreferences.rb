520a37d5a1d449baa4346271e26f45957df877fd
Nate Berkopec <nate.berkopec@gmail.com>
2014-11-02 16:35:40 -0500

    Remove incorrectly implemented part of RemoveCircularReferences
    It's impossible to have a circular object reference from a deserialized JSON string - this was a remnant of the previous implementation. This check is unnecessary (and should have been calling "json_hash.reduce" anyway).

--- lib/raven/processor/removecircularreferences.rb
+++ lib/raven/processor/removecircularreferences.rb
@@ -8,8 +8,6 @@ module Raven
         v.reduce({}) { |memo, (k, v_)| memo[k] = process(v_, visited); memo }
       elsif v.is_a?(Array)
         v.map { |v_| process(v_, visited) }
-      elsif v.is_a?(String) && json_hash = parse_json_or_nil(v)
-        v.reduce({}) { |memo, (k, v_)| memo[k] = process(v_, visited); memo }.to_json
       else
         v
       end
