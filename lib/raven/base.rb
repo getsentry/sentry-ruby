troelskn/raven-ruby

Add configurable callback for deciding whether or not to send an event

lib/raven/base.rb
@@ -98,7 +98,7 @@ module Raven
     end
 
     def capture_exception(exception, options = {})
-      send_or_skip do
+      send_or_skip(exception) do
         if evt = Event.from_exception(exception, options)
           yield evt if block_given?
           if configuration.async?
@@ -111,7 +111,7 @@ module Raven
     end
 
     def capture_message(message, options = {})
-      send_or_skip do
+      send_or_skip(message) do
         if evt = Event.from_message(message, options)
           yield evt if block_given?
           if configuration.async?
@@ -123,8 +123,14 @@ module Raven
       end
     end
 
-    def send_or_skip
-      if configuration.send_in_current_environment?
+    def send_or_skip(exc)
+      send_event_if = if configuration.send_event_if
+        configuration.send_event_if.call(*[exc])
+      else
+        true
+      end
+
+      if configuration.send_in_current_environment? && send_event_if
         yield if block_given?
       else
         configuration.log_excluded_environment_message

lib/raven/configuration.rb
@@ -80,6 +80,9 @@ module Raven
     # ActionDispatch::ShowExceptions or ActionDispatch::DebugExceptions
     attr_accessor :catch_debugged_exceptions
 
+    # Provide a configurable callback to block or send events
+    attr_accessor :send_event_if
+
     IGNORE_DEFAULT = ['ActiveRecord::RecordNotFound',
                       'ActionController::RoutingError',
                       'ActionController::InvalidAuthenticityToken',

spec/raven/raven_spec.rb
@@ -71,6 +71,20 @@ describe Raven do
     end
   end
 
+  describe '.capture_exception with a capture_event_if callback' do
+    let(:exception) { build_exception }
+
+    it 'sends the result of Event.capture_exception according to the result of send_event_if' do
+      expect(Raven).not_to receive(:send).with(event)
+
+      prior_send_event_if = Raven.configuration.send_event_if
+      Raven.configuration.send_event_if = Proc.new { |e| false }
+      expect(Raven.configuration.send_event_if).to receive(:call).with(exception)
+      Raven.capture_exception(exception, options)
+      Raven.configuration.send_event_if = prior_send_event_if
+    end
+  end
+
   describe '.annotate_exception' do
     let(:exception) { build_exception }
 
@@ -87,6 +101,6 @@ describe Raven do
     end
   end
 
-  
+
 
 end

getsentry/sentry-ruby

lib/raven/base.rb
@@ -101,7 +101,7 @@ module Raven
     end
 
     def capture_exception(exception, options = {})
-      send_or_skip do
+      send_or_skip(exception) do
         if evt = Event.from_exception(exception, options)
           yield evt if block_given?
           if configuration.async?
@@ -114,7 +114,7 @@ module Raven
     end
 
     def capture_message(message, options = {})
-      send_or_skip do
+      send_or_skip(message) do
         if evt = Event.from_message(message, options)
           yield evt if block_given?
           if configuration.async?
@@ -126,8 +126,14 @@ module Raven
       end
     end
 
-    def send_or_skip
-      if configuration.send_in_current_environment?
+    def send_or_skip(exc)
+      should_send = if configuration.should_send
+        configuration.should_send.call(*[exc])
+      else
+        true
+      end
+
+      if configuration.send_in_current_environment? && should_send
         yield if block_given?
       else
         configuration.log_excluded_environment_message

lib/raven/configuration.rb
@@ -80,6 +80,9 @@ module Raven
     # ActionDispatch::ShowExceptions or ActionDispatch::DebugExceptions
     attr_accessor :catch_debugged_exceptions
 
+    # Provide a configurable callback to block or send events
+    attr_accessor :should_send
+
     IGNORE_DEFAULT = ['ActiveRecord::RecordNotFound',
                       'ActionController::RoutingError',
                       'ActionController::InvalidAuthenticityToken',

spec/raven/raven_spec.rb
@@ -70,6 +70,20 @@ describe Raven do
     end
   end
 
+  describe '.capture_exception with a should_send callback' do
+    let(:exception) { build_exception }
+
+    it 'sends the result of Event.capture_exception according to the result of should_send' do
+      expect(Raven).not_to receive(:send).with(event)
+
+      prior_should_send = Raven.configuration.should_send
+      Raven.configuration.should_send = Proc.new { |e| false }
+      expect(Raven.configuration.should_send).to receive(:call).with(exception)
+      Raven.capture_exception(exception, options)
+      Raven.configuration.should_send = prior_should_send
+    end
+  end
+
   describe '.annotate_exception' do
     let(:exception) { build_exception }
 