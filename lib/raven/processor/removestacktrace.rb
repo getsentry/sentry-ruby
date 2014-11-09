14d3fef8bb307c27e0a1dd61ffdbf80c91c52bc0
Nate Berkopec <nate.berkopec@gmail.com>
2014-11-09 11:03:11 -0500

    Add RemoveStacktrace processor
    + move processor specs into subfolder

---
+++ lib/raven/processor/removestacktrace.rb
@@ -0,0 +1,11 @@
+module Raven
+  class Processor::RemoveStacktrace < Processor
+
+    def process(value)
+      value['exception'].delete('stacktrace') if value['exception']
+
+      value
+    end
+
+  end
+end
---
+++ spec/raven/processors/removestacktrace_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+require 'raven/processor/removestacktrace'
+
+describe Raven::Processor::RemoveStacktrace do
+  before do
+    @client = double("client")
+    @processor = Raven::Processor::RemoveStacktrace.new(@client)
+  end
+
+  it 'should remove stacktraces' do
+    data = Raven::Event.capture_exception(build_exception).to_hash
+
+    expect(data['exception']['stacktrace']).to_not eq(nil)
+    result = @processor.process(data)
+
+    expect(result['exception']['stacktrace']).to eq(nil)
+  end
+
+end
