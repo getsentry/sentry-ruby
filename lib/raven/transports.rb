bf52c12dbb14068ac48c384203998337f37a5a07
Nate Berkopec <nate.berkopec@gmail.com>
2015-02-14 10:25:36 -0500

    Centralize configuration verification

--- lib/raven/transports.rb
+++ lib/raven/transports.rb
@@ -17,10 +17,7 @@ module Raven
       protected
 
       def verify_configuration
-        raise Error.new('No server specified') unless self.configuration.server
-        raise Error.new('No public key specified') unless self.configuration.public_key
-        raise Error.new('No secret key specified') unless self.configuration.secret_key
-        raise Error.new('No project ID specified') unless self.configuration.project_id
+        configuration.verify!
       end
     end
   end
