dfe26167b1f26c165db1b9832233e25d3e7060fc
Lucas Mazza <lucastmazza@gmail.com>
2015-04-30 15:48:33 -0300

    Add support for ActiveJob.

--- lib/raven/integrations/rails.rb
+++ lib/raven/integrations/rails.rb
@@ -14,6 +14,13 @@ module Raven
       end
     end
 
+    initializer 'raven.active_job' do
+      ActiveSupport.on_load :active_job do
+        require 'raven/integrations/rails/active_job'
+        include Raven::Rails::ActiveJob
+      end
+    end
+
     config.after_initialize do
       Raven.configure do |config|
         config.logger ||= ::Rails.logger
