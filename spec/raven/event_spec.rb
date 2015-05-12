05aa03fc94dcf4f9f92687324723ab95cc459883
David Cramer <dcramer@gmail.com>
2015-05-12 15:57:06 -0700

    Add an explicit test for jRuby exceptions

--- spec/raven/event_spec.rb
+++ spec/raven/event_spec.rb
@@ -467,6 +467,23 @@ describe Raven::Event do
       end
     end
 
+    if RUBY_PLATFORM == "java"
+      context 'when running under jRuby' do
+        let(:exception) do
+          begin
+            raise java.lang.OutOfMemoryError.new("A Java error")
+          rescue Exception => e
+            return e
+          end
+        end
+
+        it 'should have a backtrace' do
+          frames = hash[:exception][:values][0][:stacktrace][:frames]
+          expect(frames.length).not_to eq(0)
+        end
+      end
+    end
+
     context 'when the exception has a backtrace' do
       let(:exception) do
         e = Exception.new(message)
