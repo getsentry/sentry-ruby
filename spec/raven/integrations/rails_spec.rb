037baabcc52c64d288bf4a90e0c513aac455e331
Nate Berkopec <nate.berkopec@gmail.com>
2015-12-21 13:36:44 -0500

    Fix Rails multi-exception reports

--- spec/raven/integrations/rails_spec.rb
+++ spec/raven/integrations/rails_spec.rb
@@ -8,17 +8,31 @@ describe TestApp, :type => :request do
       config.dsn = 'dummy://notaserver'
       config.encoding = 'json'
     end
-
+    Rails.env = "production"
     TestApp.initialize!
+  end
+
+  after(:each) do
+    Raven.client.transport.events = []
   end
 
   it "inserts middleware" do
     expect(TestApp.middleware).to include(Raven::Rack)
   end
 
-  pending "should capture exceptions" do
+  it "should capture exceptions in production" do
     get "/exception"
     expect(response.status).to eq(500)
     expect(Raven.client.transport.events.size).to eq(1)
+  end
+
+  it "should properly set the exception's URL" do
+    get "/exception"
+
+    # TODO: dummy transport shouldn't even encode the event
+    event = Raven.client.transport.events.first
+    event = JSON.parse!(event[1])
+
+    expect(event['request']['url']).to eq("http://www.example.com/exception")
   end
 end
