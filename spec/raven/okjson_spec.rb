bf901282bfb0c4d6924e58f2706b4bb4bcd03a15
Michael Sauter <michael.sauter@experteer.com>
2015-12-21 09:09:04 +0100

    Add specs for fixnum keys and values

--- spec/raven/okjson_spec.rb
+++ spec/raven/okjson_spec.rb
@@ -1,21 +1,27 @@
 require 'spec_helper'
 
 describe Raven::OkJson do
-  ['foo', :foo].each do |obj|
-    it "works with #{obj.class} keys" do
-      expect(Raven::OkJson.encode(obj => 'bar')).to eq '{"foo":"bar"}'
+  data = [
+    OpenStruct.new(:key => 'foo', :val => 'bar', :enc_key => '"foo"', :enc_val => '"bar"'),
+    OpenStruct.new(:key => :foo, :val => :bar, :enc_key => '"foo"', :enc_val => '"bar"'),
+    OpenStruct.new(:key => 1, :val => 1, :enc_key => '"1"', :enc_val => '1')
+  ]
+
+  data.each do |obj|
+    it "works with #{obj.key.class} keys" do
+      expect(Raven::OkJson.encode(obj.key => 'bar')).to eq "{#{obj.enc_key}:\"bar\"}"
     end
 
-    it "works with #{obj.class} values" do
-      expect(Raven::OkJson.encode('bar' => obj)).to eq '{"bar":"foo"}'
+    it "works with #{obj.val.class} values" do
+      expect(Raven::OkJson.encode('bar' => obj.val)).to eq "{\"bar\":#{obj.enc_val}}"
     end
 
-    it "works with an array of #{obj.class}s" do
-      expect(Raven::OkJson.encode('bar' => [obj])).to eq '{"bar":["foo"]}'
+    it "works with an array of #{obj.val.class}s" do
+      expect(Raven::OkJson.encode('bar' => [obj.val])).to eq "{\"bar\":[#{obj.enc_val}]}"
     end
 
-    it "works with a hash of #{obj.class}s" do
-      expect(Raven::OkJson.encode('bar' => {obj => obj})).to eq '{"bar":{"foo":"foo"}}'
+    it "works with a hash of #{obj.val.class}s" do
+      expect(Raven::OkJson.encode('bar' => {obj.key => obj.val})).to eq "{\"bar\":{#{obj.enc_key}:#{obj.enc_val}}}"
     end
   end
 
