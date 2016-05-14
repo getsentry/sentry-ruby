5bfe31c99c61730ddc881755e8461e0164657dc2
Phillip Baker <phillip@digitalocean.com>
2016-05-14 19:37:52 -0400

    Copy (most of) Raven base.rb to make instances available.
    A copy of Raven's base module class methods, minus some of the integration
    and global hooks since it's meant to be used explicitly. Useful for
    sending errors to multiple sentry projects in a large application.
    
    ```
    class Foo
      def initialize
        @other_raven = Raven::Instance.new
        @other_raven.configure do |config|
          config.server = 'http://...'
        end
      end
    
      def foo
        ...
      rescue => e
        @other_raven.capture_exception(e)
      end
    end
    ```
    
    Pretty brute force attempt to address https://github.com/getsentry/raven-ruby/issues/462.

---
+++ lib/raven/instance.rb
@@ -0,0 +1,203 @@
+require 'raven/base'
+
+module Raven
+  # A copy of Raven's base module class methods, minus some of the integration
+  # and global hooks since it's meant to be used explicitly. Useful for
+  # sending errors to multiple sentry projects in a large application.
+  #
+  # @example
+  #   class Foo
+  #     def initialize
+  #       @other_raven = Raven::Instance.new
+  #       @other_raven.configure do |config|
+  #         config.server = 'http://...'
+  #       end
+  #     end
+  #
+  #     def foo
+  #       # ...
+  #     rescue => e
+  #       @other_raven.capture_exception(e)
+  #     end
+  #   end
+  class Instance
+    # The client object is responsible for delivering formatted data to the
+    # Sentry server. Must respond to #send. See Raven::Client.
+    attr_writer :client
+
+    # A Raven configuration object. Must act like a hash and return sensible
+    # values for all Raven configuration options. See Raven::Configuration.
+    attr_writer :configuration
+
+    def context
+      @context ||= Context.new
+    end
+
+    def logger
+      @logger ||= Logger.new
+    end
+
+    # The configuration object.
+    # @see Raven.configure
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    # The client object is responsible for delivering formatted data to the
+    # Sentry server.
+    def client
+      @client ||= Client.new(configuration)
+    end
+
+    # Tell the log that the client is good to go
+    def report_status
+      return if client.configuration.silence_ready
+      if client.configuration.send_in_current_environment?
+        logger.info "Raven #{VERSION} ready to catch errors"
+      else
+        logger.info "Raven #{VERSION} configured not to send errors."
+      end
+    end
+    alias_method :report_ready, :report_status
+
+    # Call this method to modify defaults in your initializers.
+    #
+    # @example
+    #   Raven.configure do |config|
+    #     config.server = 'http://...'
+    #   end
+    def configure
+      yield(configuration) if block_given?
+
+      self.client = Client.new(configuration)
+      report_status
+      self.client
+    end
+
+    # Send an event to the configured Sentry server
+    #
+    # @example
+    #   evt = Raven::Event.new(:message => "An error")
+    #   Raven.send_event(evt)
+    def send_event(event)
+      client.send_event(event)
+    end
+
+    # Capture and process any exceptions from the given block.
+    #
+    # @example
+    #   Raven.capture do
+    #     MyApp.run
+    #   end
+    def capture(options = {})
+      begin
+        yield
+      rescue Error
+        raise # Don't capture Raven errors
+      rescue Exception => e
+        capture_exception(e, options)
+        raise
+      end
+    end
+
+    def capture_type(obj, options = {})
+      return false unless should_capture?(obj)
+      message_or_exc = obj.is_a?(String) ? "message" : "exception"
+      if (evt = Event.send("from_" + message_or_exc, obj, options))
+        yield evt if block_given?
+        if configuration.async?
+          configuration.async.call(evt)
+        else
+          send_event(evt)
+        end
+        Thread.current["sentry_#{object_id}_last_event_id".to_sym] = evt.id
+        evt
+      end
+    end
+    alias_method :capture_message, :capture_type
+    alias_method :capture_exception, :capture_type
+
+    def last_event_id
+      Thread.current["sentry_#{object_id}_last_event_id".to_sym]
+    end
+
+    def should_capture?(message_or_exc)
+      if configuration.should_capture
+        configuration.should_capture.call(*[message_or_exc])
+      else
+        true
+      end
+    end
+
+    # Provides extra context to the exception prior to it being handled by
+    # Raven. An exception can have multiple annotations, which are merged
+    # together.
+    #
+    # The options (annotation) is treated the same as the ``options``
+    # parameter to ``capture_exception`` or ``Event.from_exception``, and
+    # can contain the same ``:user``, ``:tags``, etc. options as these
+    # methods.
+    #
+    # These will be merged with the ``options`` parameter to
+    # ``Event.from_exception`` at the top of execution.
+    #
+    # @example
+    #   begin
+    #     raise "Hello"
+    #   rescue => exc
+    #     Raven.annotate_exception(exc, :user => { 'id' => 1,
+    #                              'email' => 'foo@example.com' })
+    #   end
+    def annotate_exception(exc, options = {})
+      notes = (exc.instance_variable_defined?(:@__raven_context) && exc.instance_variable_get(:@__raven_context)) || {}
+      notes.merge!(options)
+      exc.instance_variable_set(:@__raven_context, notes)
+      exc
+    end
+
+    # Bind user context. Merges with existing context (if any).
+    #
+    # It is recommending that you send at least the ``id`` and ``email``
+    # values. All other values are arbitrary.
+    #
+    # @example
+    #   Raven.user_context('id' => 1, 'email' => 'foo@example.com')
+    def user_context(options = nil)
+      context.user = options || {}
+    end
+
+    # Bind tags context. Merges with existing context (if any).
+    #
+    # Tags are key / value pairs which generally represent things like
+    # application version, environment, role, and server names.
+    #
+    # @example
+    #   Raven.tags_context('my_custom_tag' => 'tag_value')
+    def tags_context(options = nil)
+      context.tags.merge!(options || {})
+    end
+
+    # Bind extra context. Merges with existing context (if any).
+    #
+    # Extra context shows up as Additional Data within Sentry, and is
+    # completely arbitrary.
+    #
+    # @example
+    #   Raven.extra_context('my_custom_data' => 'value')
+    def extra_context(options = nil)
+      context.extra.merge!(options || {})
+    end
+
+    def rack_context(env)
+      env = nil if env.empty?
+
+      context.rack_env = env
+    end
+
+    # For cross-language compat
+    alias :captureException :capture_exception
+    alias :captureMessage :capture_message
+    alias :annotateException :annotate_exception
+    alias :annotate :annotate_exception
+  end
+end
