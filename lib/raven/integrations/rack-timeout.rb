3e5fca903670f92e8c18ec137cefa872aba0e2c3
Nate Berkopec <nate.berkopec@gmail.com>
2016-09-06 16:31:23 -0700

    Integrate rack-timeout

---
+++ lib/raven/integrations/rack-timeout.rb
@@ -0,0 +1,14 @@
+require 'rack-timeout'
+
+# This integration is a good example of how to change how exceptions
+# get grouped by Sentry's UI. Simply override #raven_context in
+# the exception class, and append something to the fingerprint
+# that will distinguish exceptions in the way you desire.
+module RackTimeoutExtensions
+  def raven_context
+    { fingerprint: ["{{ default }}", env["REQUEST_URI"]] }
+  end
+end
+
+Rack::Timeout::Error.include RackTimeoutExtensions
+Rack::Timeout::RequestTimeoutException.include RackTimeoutExtensions
