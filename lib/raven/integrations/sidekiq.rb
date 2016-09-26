ded4ee7c341f847725d001e742669b06fe1df183
Nate Berkopec <nate.berkopec@gmail.com>
2016-09-26 11:23:36 -0600

    Sidekiq: identify culprit

--- lib/raven/integrations/sidekiq.rb
+++ lib/raven/integrations/sidekiq.rb
@@ -3,56 +3,57 @@ require 'sidekiq'
 
 module Raven
   class Sidekiq
-    def call(_worker, msg, _queue)
-      started_at = Time.now
-      yield
-    rescue Exception => ex
-      Raven.capture_exception(ex, :extra => { :sidekiq => msg },
-                                  :time_spent => Time.now - started_at)
-      raise
-    ensure
-      Context.clear!
-      BreadcrumbBuffer.clear!
+    ACTIVEJOB_RESERVED_PREFIX = "_aj_".freeze
+
+    def call(ex, context)
+      context = filter_context(context)
+      Raven.capture_exception(
+        ex,
+        :message => ex.message,
+        :extra => { :sidekiq => context },
+        :culprit => culprit_from_context(context)
+      )
+    end
+
+    private
+
+    # Once an ActiveJob is queued, ActiveRecord references get serialized into
+    # some internal reserved keys, such as _aj_globalid.
+    #
+    # The problem is, if this job in turn gets queued back into ActiveJob with
+    # these magic reserved keys, ActiveJob will throw up and error. We want to
+    # capture these and mutate the keys so we can sanely report it.
+    def filter_context(context)
+      case context
+      when Array
+        context.map { |arg| filter_context(arg) }
+      when Hash
+        Hash[context.map { |key, value| filter_context_hash(key, value) }]
+      else
+        context
+      end
+    end
+
+    def filter_context_hash(key, value)
+      (key = key[3..-1]) if key [0..3] == ACTIVEJOB_RESERVED_PREFIX
+      [key, filter_context(value)]
+    end
+
+    def culprit_from_context(context)
+      classname = (context["class"] || (context["job"] && context["job"]["class"]))
+      if classname
+        "Sidekiq/#{classname}"
+      elsif context["event"]
+        "Sidekiq/#{context['event']}"
+      else
+        "Sidekiq"
+      end
     end
   end
 end
 
-if Sidekiq::VERSION < '3'
-  # old behavior
-  ::Sidekiq.configure_server do |config|
-    config.server_middleware do |chain|
-      chain.add ::Raven::Sidekiq
-    end
-  end
-else
+if Sidekiq::VERSION > '3'
   Sidekiq.configure_server do |config|
-    config.error_handlers << proc do |ex, context|
-      Raven.capture_exception(ex, :extra => {
-                                :sidekiq => filter_context(context)
-                              })
-    end
+    config.error_handlers << Raven::Sidekiq.new
   end
-end
-
-def filter_context(context)
-  case context
-  when Array
-    context.map { |arg| filter_context(arg) }
-  when Hash
-    Hash[context.map { |key, value| filter_context_hash(key, value) }]
-  else
-    context
-  end
-end
-
-def filter_context_hash(key, value)
-  # Strip any `_aj` prefixes from keys.
-  # These keys come from an internal serialized object from ActiveJob.
-  # Internally, there are a subset of keys that ActiveJob references, but
-  # these are declared as private, and I don't think it's wise
-  # to keep chasing what this list is. But they all use a common prefix, so
-  # we want to strip this becuase ActiveJob will complain.
-  # e.g.: _aj_globalid -> _globalid
-  (key = key[3..-1]) if key [0..3] == "_aj_"
-  [key, filter_context(value)]
 end
