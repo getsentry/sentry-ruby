d0d5845eeca396b07b90f899a4ac8e1e9acc171a
Nate Berkopec <nate.berkopec@gmail.com>
2016-10-11 09:57:02 -0600

    Add more OS info

--- lib/raven/context.rb
+++ lib/raven/context.rb
@@ -14,26 +14,34 @@ module Raven
 
     def initialize
       self.extra = {}
-      self.os = os_context
+      self.os = self.class.os_context
       self.rack_env = nil
-      self.runtime = runtime_context
+      self.runtime = self.class.runtime_context
       self.tags = {}
       self.user = {}
     end
 
-    private
+    class << self
+      def os_context
+        @os_context ||= {
+          "name" => sys_command("uname -s") || RbConfig::CONFIG["host_os"],
+          "version" => sys_command("uname -v"),
+          "build" => sys_command("uname -r"),
+          "kernel_version" => sys_command("uname -a", "ver")
+        }
+      end
 
-    def os_context
-      {
-        "name" => RbConfig::CONFIG["host_os"]
-      }
-    end
+      def runtime_context
+        @runtime_context ||= {
+          "name" => RbConfig::CONFIG["ruby_install_name"],
+          "version" => RbConfig::CONFIG["ruby_version"]
+        }
+      end
 
-    def runtime_context
-      {
-        "name" => RbConfig::CONFIG["ruby_install_name"],
-        "version" => RbConfig::CONFIG["ruby_version"]
-      }
+      def sys_command(linux_command, win_command = nil)
+        `#{linux_command}` rescue nil ||
+          `#{win_command}` rescue nil
+      end
     end
   end
 end
