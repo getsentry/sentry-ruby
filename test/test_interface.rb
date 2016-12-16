99e04e3dbbdd7c9d2654c5d4982a0bc613e26935
Nate Berkopec <nate.berkopec@gmail.com>
2016-12-16 08:23:11 -0700

    Test interfaces

---
+++ test/test_interface.rb
@@ -0,0 +1,26 @@
+require_relative 'helper'
+
+class TestInterface < Raven::Interface
+  attr_accessor :foo
+end
+
+class InterfaceTest < Minitest::Spec
+  it "accepts attributes when initialized" do
+    int = TestInterface.new(:foo => :bar)
+    assert_equal :bar, int.foo
+  end
+
+  it "yields during init" do
+    int = TestInterface.new { |i| i.foo = :bar }
+    assert_equal :bar, int.foo
+  end
+
+  it "registers its inheritance" do
+    assert_includes Raven::Interface.registered, :test
+  end
+
+  it "converts instance variables to a hash" do
+    int = TestInterface.new(foo: :bar)
+    assert_equal({ :foo => :bar }, int.to_hash)
+  end
+end
