25b8a9fd31ff901bbb4386ffa06c896236a86bdf
Kirill Zaitsev <kirik910@gmail.com>
2017-01-07 21:34:45 +0200

    Show top level tasks instead proocess name

--- lib/raven/integrations/rake.rb
+++ lib/raven/integrations/rake.rb
@@ -6,7 +6,7 @@ module Rake
   class Application
     alias orig_display_error_messsage display_error_message
     def display_error_message(ex)
-      Raven.capture_exception ex, :logger => 'rake', :tags => { 'rake_task' => @name }
+      Raven.capture_exception ex, :logger => 'rake', :tags => { 'rake_task' => top_level_tasks.join(' ') }
       orig_display_error_messsage(ex)
     end
   end
