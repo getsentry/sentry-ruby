eeb08fba9d388f49dbf3604798fdb25548dd548c
andrew morton <drewish@katherinehouse.com>
2017-01-30 23:02:41 -0700

    Don't assume exception has a response
    The wrapped OpenSSL exceptions don't have a response:
    
        #<Faraday::SSLError wrapped=#<OpenSSL::SSL::SSLError: SSL_connect returned=1 errno=0 state=error: certificate verify failed>>
    
    Follow up to #585.

--- lib/raven/transports/http.rb
+++ lib/raven/transports/http.rb
@@ -29,7 +29,9 @@ module Raven
         end
       rescue Faraday::ClientError => ex
         error_info = ex.message
-        error_info += " Error in headers is: #{ex.response[:headers]['x-sentry-error']}" if ex.response[:headers]['x-sentry-error']
+        if ex.response && ex.response[:headers]['x-sentry-error']
+          error_info += " Error in headers is: #{ex.response[:headers]['x-sentry-error']}"
+        end
         raise Raven::Error, error_info
       end
 
