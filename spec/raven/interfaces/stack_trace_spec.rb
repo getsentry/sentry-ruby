a213d0bbfbd56fe4ae113b91dbe1141cd12dc3cd
Nate Berkopec <nate.berkopec@gmail.com>
2017-05-24 07:41:15 -0600

    over $:

--- spec/raven/interfaces/stack_trace_spec.rb
+++ spec/raven/interfaces/stack_trace_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe Raven::StacktraceInterface::Frame do
   it "should convert pathnames to strings" do
     frame = Raven::StacktraceInterface::Frame.new
-    $:.unshift Pathname.pwd # Oh no, a Pathname in the $LOAD_PATH!
+    $LOAD_PATH.unshift Pathname.pwd # Oh no, a Pathname in the $LOAD_PATH!
     frame.abs_path = __FILE__
     expect(frame.filename).to match(/stack_trace_spec.rb/)
-    $:.shift
+    $LOAD_PATH.shift
   end
 end
