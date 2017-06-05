c8abf022cec526c43aa6de4e4b560ce9c6148e02
Nate Berkopec <nate.berkopec@gmail.com>
2017-06-05 08:52:02 -0600

    Add small amount of extra Backtrace test coverage

--- spec/raven/backtrace_spec.rb
+++ spec/raven/backtrace_spec.rb
@@ -5,7 +5,20 @@ describe Raven::Backtrace do
     @backtrace = Raven::Backtrace.parse(Thread.current.backtrace)
   end
 
+  it "#lines" do
+    expect(@backtrace.lines.first).to be_a(Raven::Backtrace::Line)
+  end
+
   it "#inspect" do
     expect(@backtrace.inspect).to match(/Backtrace: .*>$/)
+  end
+
+  it "#to_s" do
+    expect(@backtrace.to_s).to match(/backtrace_spec.rb:5/)
+  end
+
+  it "==" do
+    @backtrace2 = Raven::Backtrace.new(@backtrace.lines)
+    expect(@backtrace).to be == @backtrace2
   end
 end
