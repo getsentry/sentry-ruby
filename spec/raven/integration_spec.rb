41f8e3aa7f69cbcca88d12ec0365a999ed6cd5f8
Nate Berkopec <nate.berkopec@gmail.com>
2017-06-14 10:45:56 -0600

    Cleanup integration tests

--- spec/raven/integration_spec.rb
+++ spec/raven/integration_spec.rb
@@ -1,83 +1,57 @@
 require 'spec_helper'
 
 describe "Integration tests" do
-  example "posting an exception" do
-    stubs = Faraday::Adapter::Test::Stubs.new do |stub|
-      stub.post('sentry/api/42/store/') { [200, {}, 'ok'] }
-    end
-    io = StringIO.new
-
-    Raven.configure do |config|
+  before(:each) do
+    @io = StringIO.new
+    @logger = Logger.new(@io)
+    @instance = Raven::Instance.new
+    @stubs = Faraday::Adapter::Test::Stubs.new
+    @instance.configuration = Raven::Configuration.new.tap do |config|
       config.server = 'http://12345:67890@sentry.localdomain/sentry/42'
-      config.http_adapter = [:test, stubs]
-      config.logger = Logger.new(io)
+      config.http_adapter = [:test, @stubs]
+      config.logger = @logger
     end
+  end
 
-    Raven.capture_exception(build_exception)
+  example "posting an exception" do
+    @stubs.post('sentry/api/42/store/') { [200, {}, 'ok'] }
 
-    stubs.verify_stubbed_calls
+    @instance.capture_exception(build_exception)
 
-    expect(io.string).to match(/Sending event [0-9a-f]+ to Sentry$/)
+    @stubs.verify_stubbed_calls
+    expect(@io.string).to match(/Sending event [0-9a-f]+ to Sentry$/)
   end
 
   example "posting an exception to a prefixed DSN" do
-    stubs = Faraday::Adapter::Test::Stubs.new do |stub|
-      stub.post('/prefix/sentry/api/42/store/') { [200, {}, 'ok'] }
-    end
+    @stubs.post('/prefix/sentry/api/42/store/') { [200, {}, 'ok'] }
+    @instance.configuration.server = 'http://12345:67890@sentry.localdomain/prefix/sentry/42'
 
-    Raven.configure do |config|
-      config.server = 'http://12345:67890@sentry.localdomain/prefix/sentry/42'
-      config.http_adapter = [:test, stubs]
-    end
+    @instance.capture_exception(build_exception)
 
-    Raven.capture_exception(build_exception)
-
-    stubs.verify_stubbed_calls
+    @stubs.verify_stubbed_calls
   end
 
   example "hitting quota limit shouldn't swallow exception" do
-    stubs = Faraday::Adapter::Test::Stubs.new do |stub|
-      stub.post('sentry/api/42/store/') { [403, {}, 'Creation of this event was blocked'] }
-    end
-
-    logger = Logger.new(StringIO.new)
-
-    Raven.configure do |config|
-      config.server = 'http://12345:67890@sentry.localdomain/sentry/42'
-      config.http_adapter = [:test, stubs]
-      config.logger = logger
-    end
+    @stubs.post('sentry/api/42/store/') { [403, {}, 'Creation of this event was blocked'] }
 
     # sentry error and original error
-    expect(logger).not_to receive(:error).twice
-    Raven.capture_exception(build_exception)
+    expect(@logger).not_to receive(:error).twice
+    @instance.capture_exception(build_exception)
 
-    stubs.verify_stubbed_calls
+    @stubs.verify_stubbed_calls
   end
 
   example "timed backoff should prevent sends" do
-    io = StringIO.new
-    Raven.configure do |config|
-      config.server = 'http://12345:67890@sentry.localdomain/sentry/42'
-      config.http_adapter = [:test, nil]
-      config.logger = Logger.new(io)
-    end
-
-    expect_any_instance_of(Raven::Transports::HTTP).to receive(:send_event).exactly(1).times.and_raise(Faraday::Error::ConnectionFailed, "conn failed")
-    2.times { Raven.capture_exception(build_exception) }
-    expect(io.string).to match(/Failed to submit event: ZeroDivisionError: divided by 0$/)
+    expect(@instance.client.transport).to receive(:send_event).exactly(1).times.and_raise(Faraday::Error::ConnectionFailed, "conn failed")
+    2.times { @instance.capture_exception(build_exception) }
+    expect(@io.string).to match(/Failed to submit event: ZeroDivisionError: divided by 0$/)
   end
 
   example "transport failure should call transport_failure_callback" do
-    io = StringIO.new
-    Raven.configure do |config|
-      config.server = 'http://12345:67890@sentry.localdomain/sentry/42'
-      config.http_adapter = [:test, nil]
-      config.transport_failure_callback = proc { |_e| io.puts "OK!" }
-    end
+    @instance.configuration.transport_failure_callback = proc { |_e| @io.puts "OK!" }
 
-    expect_any_instance_of(Raven::Transports::HTTP).to receive(:send_event).exactly(1).times.and_raise(Faraday::Error::ConnectionFailed, "conn failed")
-    Raven.capture_exception(build_exception)
-    expect(io.string).to match(/OK!$/)
+    expect(@instance.client.transport).to receive(:send_event).exactly(1).times.and_raise(Faraday::Error::ConnectionFailed, "conn failed")
+    @instance.capture_exception(build_exception)
+    expect(@io.string).to match(/OK!$/)
   end
 end
