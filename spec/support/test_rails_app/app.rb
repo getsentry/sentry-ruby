18fafd655a8a85540602419234e22b32949d9400
Nate Berkopec <nate.berkopec@gmail.com>
2017-07-11 16:13:05 -0600

    Proper ActiveJob integration.
    Close #709
    Close #671

--- spec/support/test_rails_app/app.rb
+++ spec/support/test_rails_app/app.rb
@@ -1,10 +1,9 @@
 require 'rails'
-# require "active_model/railtie"
-# require "active_job/railtie"
 # require "active_record/railtie"
+require "action_view/railtie"
 require "action_controller/railtie"
 # require "action_mailer/railtie"
-require "action_view/railtie"
+require "active_job/railtie"
 # require "action_cable/engine"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
