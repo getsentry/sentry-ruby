de8a832cb7e3dd5970e7433a940728abb0cbfff6
Nate Berkopec <nate.berkopec@gmail.com>
2017-10-11 09:05:24 -0600

    Cleanup conditional on debug catcher tests

--- spec/raven/integrations/rails/overrides/debug_exceptions_catcher_spec.rb
+++ spec/raven/integrations/rails/overrides/debug_exceptions_catcher_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe "Raven::Rails::Overrides::DebugExceptionsCatcher", :rails => true
 
   let(:env) { {} }
 
-  if defined?(Rails) && Rails.version < "5.1.0"
+  if Class.respond_to?(:alias_method_chain)
     context "using include" do
       before do
         middleware.send(:include, Raven::Rails::Overrides::OldDebugExceptionsCatcher)
@@ -53,25 +53,26 @@ RSpec.describe "Raven::Rails::Overrides::DebugExceptionsCatcher", :rails => true
     end
   end
 
-  context "using prepend" do
-    before do
-      skip "prepend not available" unless middleware.respond_to?(:prepend, true)
-      middleware.send(:prepend, Raven::Rails::Overrides::DebugExceptionsCatcher)
-    end
+  if Class.respond_to?(:prepend)
+    context "using prepend" do
+      before do
+        middleware.send(:prepend, Raven::Rails::Overrides::DebugExceptionsCatcher)
+      end
 
-    it "shows the exception" do
-      expect(middleware.new(app).call(env)).to eq([500, "app error", {}])
-    end
-
-    it "captures the exception" do
-      expect(Raven::Rack).to receive(:capture_exception)
-      middleware.new(app).call(env)
-    end
-
-    context "when an error is raised" do
-      it "shows the original exception" do
-        allow(Raven::Rack).to receive(:capture_exception).and_raise("raven error")
+      it "shows the exception" do
         expect(middleware.new(app).call(env)).to eq([500, "app error", {}])
+      end
+
+      it "captures the exception" do
+        expect(Raven::Rack).to receive(:capture_exception)
+        middleware.new(app).call(env)
+      end
+
+      context "when an error is raised" do
+        it "shows the original exception" do
+          allow(Raven::Rack).to receive(:capture_exception).and_raise("raven error")
+          expect(middleware.new(app).call(env)).to eq([500, "app error", {}])
+        end
       end
     end
   end
