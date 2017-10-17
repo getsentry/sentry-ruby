50c6c4c29a5616a4887621cd274460468ca0e29c
Nate Berkopec <nate.berkopec@gmail.com>
2017-10-17 13:44:13 -0600

    DRY up the debug catcher spec

--- spec/raven/integrations/rails/overrides/debug_exceptions_catcher_spec.rb
+++ spec/raven/integrations/rails/overrides/debug_exceptions_catcher_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe "Raven::Rails::Overrides::DebugExceptionsCatcher", :rails => true do
-  before(:all) do
-    require 'raven/integrations/rails/overrides/debug_exceptions_catcher'
-  end
-
+RSpec.shared_examples "exception catching middleware" do |parameter|
   let(:middleware) do
     Class.new do
       def initialize(app)
@@ -29,27 +25,35 @@ RSpec.describe "Raven::Rails::Overrides::DebugExceptionsCatcher", :rails => true
 
   let(:env) { {} }
 
+  it "shows the exception" do
+    expect(middleware.new(app).call(env)).to eq([500, "app error", {}])
+  end
+
+  it "captures the exception" do
+    expect(Raven::Rack).to receive(:capture_exception)
+    middleware.new(app).call(env)
+  end
+
+  context "when an error is raised" do
+    it "shows the original exception" do
+      allow(Raven::Rack).to receive(:capture_exception).and_raise("raven error")
+      expect(middleware.new(app).call(env)).to eq([500, "app error", {}])
+    end
+  end
+end
+
+RSpec.describe "Raven::Rails::Overrides::DebugExceptionsCatcher", :rails => true do
+  before(:all) do
+    require 'raven/integrations/rails/overrides/debug_exceptions_catcher'
+  end
+
   if Class.respond_to?(:alias_method_chain)
     context "using include" do
       before do
         middleware.send(:include, Raven::Rails::Overrides::OldDebugExceptionsCatcher)
       end
 
-      it "shows the exception" do
-        expect(middleware.new(app).call(env)).to eq([500, "app error", {}])
-      end
-
-      it "captures the exception" do
-        expect(Raven::Rack).to receive(:capture_exception)
-        middleware.new(app).call(env)
-      end
-
-      context "when an error is raised" do
-        it "shows the original exception" do
-          allow(Raven::Rack).to receive(:capture_exception).and_raise("raven error")
-          expect(middleware.new(app).call(env)).to eq([500, "app error", {}])
-        end
-      end
+      include_examples "exception catching middleware"
     end
   end
 
@@ -59,21 +63,7 @@ RSpec.describe "Raven::Rails::Overrides::DebugExceptionsCatcher", :rails => true
         middleware.send(:prepend, Raven::Rails::Overrides::DebugExceptionsCatcher)
       end
 
-      it "shows the exception" do
-        expect(middleware.new(app).call(env)).to eq([500, "app error", {}])
-      end
-
-      it "captures the exception" do
-        expect(Raven::Rack).to receive(:capture_exception)
-        middleware.new(app).call(env)
-      end
-
-      context "when an error is raised" do
-        it "shows the original exception" do
-          allow(Raven::Rack).to receive(:capture_exception).and_raise("raven error")
-          expect(middleware.new(app).call(env)).to eq([500, "app error", {}])
-        end
-      end
+      include_examples "exception catching middleware"
     end
   end
 end
