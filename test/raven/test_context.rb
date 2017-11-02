e3ee69fa387223aa9067bc841700f2b8fb709c70
Nate Berkopec <nate.berkopec@gmail.com>
2017-11-02 14:46:16 -0600

    Context tests

---
+++ test/raven/test_context.rb
@@ -0,0 +1,55 @@
+require 'test_helper'
+
+class RavenContextTest < Raven::Test
+  it "manages thread context" do
+    thread_ctx = Raven::Context.current
+    assert_instance_of Raven::Context, thread_ctx
+    assert_equal thread_ctx.object_id, Raven::Context.current.object_id
+
+    Raven::Context.clear!
+    refute_equal thread_ctx.object_id, Raven::Context.current.object_id
+  end
+
+  it "has a new context for each thread" do
+    thread_ctx = Raven::Context.current
+    t = Thread.new { Thread.current[:context] = Raven::Context.current }
+    t.join
+
+    refute_equal thread_ctx.object_id, t[:context].object_id
+  end
+
+  it "merges extra context with server" do
+    @ctx = Raven::Context.new
+
+    assert @ctx.extra.keys.include?(:server)
+
+    @ctx.extra[:my_key] = :my_val
+
+    assert_equal :my_val, @ctx.extra[:my_key]
+    assert @ctx.extra.keys.include?(:server)
+  end
+end
+
+class RavenOSContextTest < Raven::ThreadUnsafeTest
+  it "has os context" do
+    ctx = Raven::Context.new
+    sys = Minitest::Mock.new
+    sys.expect :command, "foo", ["uname -s"]
+    sys.expect :command, "foo", ["uname -v"]
+    sys.expect :command, "foo", ["uname -r"]
+    sys.expect :command, "foo", ["uname -a"]
+
+    Raven::Context.stub(:sys, sys) do
+      ctx.extra
+    end
+
+    sys.verify
+  end
+
+  it "has runtime context" do
+    ctx = Raven::Context.new
+
+    assert_equal RUBY_ENGINE, ctx.extra[:server][:runtime][:name]
+    assert_equal RUBY_DESCRIPTION, ctx.extra[:server][:runtime][:version]
+  end
+end
