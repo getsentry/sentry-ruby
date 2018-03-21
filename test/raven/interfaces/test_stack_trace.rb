efa91abcb569eba126875acde3d3a733d0cd0438
Nate Berkopec <nate.berkopec@gmail.com>
2018-03-21 09:45:39 -0600

    Fix test file name

--- test/raven/interfaces/test_stack_trace.rb
+++ test/raven/interfaces/test_stack_trace.rb
@@ -4,9 +4,9 @@ class StacktraceInterfaceTest < Raven::Test
   def setup
     line = OpenStruct.new(
       :lineno => 2,
-      :to_s => "test/support/test_file.rb:2:in `my_method'",
-      :path => "test/support/test_file.rb",
-      :absolute_path => "#{Dir.pwd}/test/support/test_file.rb"
+      :to_s => "test/support/example_file.rb:2:in `my_method'",
+      :path => "test/support/example_file.rb",
+      :absolute_path => "#{Dir.pwd}/test/support/example_file.rb"
     )
     @frame = Raven::StacktraceInterface::Frame.new(:line => line)
   end
