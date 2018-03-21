ce150d1c7689ee9cd5185bd34c41ed230c1e0d9e
Nate Berkopec <nate.berkopec@gmail.com>
2018-03-21 09:44:48 -0600

    Add Rack::Timeout test

---
+++ test/raven/integrations/test_rack-timeout.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+require "rack/timeout/base"
+require "raven/integrations/rack-timeout"
+
+class RavenRackTimeoutTest < Raven::Test
+  it "should have a raven_context method defined" do
+    exc = Rack::Timeout::RequestTimeoutException.new("REQUEST_URI" => "This is a URI")
+    assert_equal ["{{ default }}", "This is a URI"], exc.raven_context[:fingerprint]
+  end
+end
