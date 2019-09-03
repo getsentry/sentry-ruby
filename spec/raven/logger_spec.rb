ff84c326b18b3648fa07abb8cd73dc57005a8bb0
Gavin Mogan <gmogan@cloudbees.com>
2019-09-03 16:46:27 -0700

    Logger can take in exceptions according to documentation so stringify first
    Docs: https://ruby-doc.org/stdlib-2.5.0/libdoc/logger/rdoc/Logger.html\#method-i-add-label-Args

--- spec/raven/logger_spec.rb
+++ spec/raven/logger_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe Raven::Logger do
 
     expect(stringio.string).to end_with("FATAL -- sentry: ** [Raven] Oh noes!\n")
   end
+
+  it "should allow exceptions to be logged" do
+    stringio = StringIO.new
+    log = Raven::Logger.new(stringio)
+
+    log.fatal(Exception.new("Oh exceptions"))
+
+    expect(stringio.string).to end_with("FATAL -- sentry: ** [Raven] Oh exceptions\n")
+  end
 end
