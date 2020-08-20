6ca2cafd3d84ec8b189a97367388cc87a3282a59
Daniel Griesser <daniel.griesser.86@gmail.com>
2020-08-20 11:21:01 +0200

    ref: Update script

--- .scripts/bump-version.sh
+++ .scripts/bump-version.sh
@@ -4,6 +4,6 @@ file_names = ['lib/raven/version.rb']
 
 file_names.each do |file_name|
   text = File.read(file_name)
-  new_contents = text.gsub(ARGV[0], ARGV[1])
+  new_contents = text.gsub(/\d.\d.\d/, ARGV[1])
   File.open(file_name, "w") {|file| file.puts new_contents }
 end
