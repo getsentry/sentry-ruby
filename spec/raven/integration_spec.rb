alexandrz/raven-ruby

Add some error context in transport_failure_callback (#796)
* Correct docs re: transactions

* Updated the client failed_send method to include HTTP error when using transport_failure_callback proc

* Updated Readme about transport_failure_callback usage

* Fixed conflicts with v3 previous updates

* Updated Readme

spec/raven/integration_spec.rb
@@ -48,10 +48,10 @@ RSpec.describe "Integration tests" do
   end
 
   it "transport failure should call transport_failure_callback" do
-    @instance.configuration.transport_failure_callback = proc { |_e| @io.puts "OK!" }
+    @instance.configuration.transport_failure_callback = proc { |event, error| @io.puts "OK! - #{error.message}" }
 
     expect(@instance.client.transport).to receive(:send_event).exactly(1).times.and_raise(Faraday::Error::ConnectionFailed, "conn failed")
     @instance.capture_exception(build_exception)
-    expect(@io.string).to match(/OK!$/)
+    expect(@io.string).to match(/OK! - conn failed$/)
   end
 end

getsentry/sentry-ruby

spec/raven/integration_spec.rb
@@ -49,11 +49,11 @@ RSpec.describe "Integration tests" do
   end
 
   it "transport failure should call transport_failure_callback" do
-    @instance.configuration.transport_failure_callback = proc { |_e| @io.puts "OK!" }
+    @instance.configuration.transport_failure_callback = proc { |_event, error| @io.puts "OK! - #{error.message}" }
 
     expect(@instance.client.transport).to receive(:send_event).exactly(1).times.and_raise(Faraday::ConnectionFailed, "conn failed")
     @instance.capture_exception(build_exception)
-    expect(@io.string).to match(/OK!$/)
+    expect(@io.string).to match(/OK! - conn failed$/)
   end
 
   describe '#before_send' do