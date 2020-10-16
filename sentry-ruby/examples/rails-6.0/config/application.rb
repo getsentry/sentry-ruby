1819c5a0d05a99dc1367b79b837971a60a77b116
st0012 <stan001212@gmail.com>
2020-10-16 20:35:35 +0800

    Update rack integration for breadcrumbs

--- sentry-ruby/examples/rails-6.0/config/application.rb
+++ sentry-ruby/examples/rails-6.0/config/application.rb
@@ -23,6 +23,7 @@ module Rails60
     config.middleware.insert_after ActionDispatch::DebugExceptions, Sentry::Rack::CaptureException
 
     Sentry.init do |config|
+      config.breadcrumbs_logger = [:sentry_logger]
       config.dsn = 'https://2fb45f003d054a7ea47feb45898f7649@o447951.ingest.sentry.io/5434472'
     end
   end
