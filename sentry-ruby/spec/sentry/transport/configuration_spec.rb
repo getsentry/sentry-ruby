1f8c058a78c2498ebb1a13468ab9966ff88b0742
Stan Lo <stan001212@gmail.com>
2020-10-17 17:44:02 +0800

    Support specifying custom transport class (#1067)
    ```ruby
    Sentry.init do |config|
      config.transport.transport_class = MyTransportClass
    end
    ```

--- sentry-ruby/spec/sentry/transport/configuration_spec.rb
+++ sentry-ruby/spec/sentry/transport/configuration_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe Sentry::Transport::Configuration do
       expect(subject.encoding).to eq("json")
     end
   end
+
+  describe "#transport_class=" do
+    it "doesn't accept non-class argument" do
+      expect { subject.transport_class = "foo" }.to raise_error(Sentry::Error, "config.transport.transport_class must a class. got: String")
+    end
+
+    it "accepts class argument" do
+      subject.transport_class = Sentry::DummyTransport
+
+      expect(subject.transport_class).to eq(Sentry::DummyTransport)
+    end
+  end
 end
