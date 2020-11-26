84d6f5d70980ba9dfaf14a111fd0d32522ab8368
st0012 <stan001212@gmail.com>
2020-11-27 00:26:34 +0800

    Add Transaction#set_initial_sample_desicion

--- sentry-ruby/lib/sentry/transaction.rb
+++ sentry-ruby/lib/sentry/transaction.rb
@@ -8,6 +8,7 @@ module Sentry
       "[ \t]*$"  # whitespace
     )
     UNLABELD_NAME = "<unlabeled transaction>".freeze
+    MESSAGE_PREFIX = "[Tracing]"
 
     attr_reader :name, :parent_sampled
 
@@ -35,6 +36,56 @@ module Sentry
       hash
     end
 
+    def set_initial_sample_desicion(sampling_context = {})
+      unless Sentry.configuration.tracing_enabled?
+        @sampled = false
+        return
+      end
+
+      return unless @sampled.nil?
+
+      transaction_description = generate_transaction_description
+
+      logger = Sentry.configuration.logger
+      sample_rate = Sentry.configuration.traces_sample_rate
+      traces_sampler = Sentry.configuration.traces_sampler
+
+      if traces_sampler.is_a?(Proc)
+        sampling_context = sampling_context.merge(
+          parent_sampled: @parent_sampled,
+          transaction_context: self.to_hash
+        )
+
+        sample_rate = traces_sampler.call(sampling_context)
+      end
+
+      unless [true, false].include?(sample_rate) || (sample_rate.is_a?(Float) && sample_rate >= 0.0 && sample_rate <= 1.0)
+        @sampled = false
+        logger.warn("#{MESSAGE_PREFIX} Discarding #{transaction_description} because of invalid sample_rate: #{sample_rate}")
+        return
+      end
+
+      if sample_rate == 0.0 || sample_rate == false
+        @sampled = false
+        logger.debug("#{MESSAGE_PREFIX} Discarding #{transaction_description} because traces_sampler returned 0 or false")
+        return
+      end
+
+      if sample_rate == true
+        @sampled = true
+      else
+        @sampled = Random.rand < sample_rate
+      end
+
+      if @sampled
+        logger.debug("#{MESSAGE_PREFIX} Starting #{transaction_description}")
+      else
+        logger.debug(
+          "#{MESSAGE_PREFIX} Discarding #{transaction_description} because it's not included in the random sample (sampling rate = #{sample_rate})"
+        )
+      end
+    end
+
     def finish(hub: nil)
       super() # Span#finish doesn't take arguments
 
@@ -47,6 +98,15 @@ module Sentry
       hub ||= Sentry.get_current_hub
       event = hub.current_client.event_from_transaction(self)
       hub.capture_event(event)
+    end
+
+    private
+
+    def generate_transaction_description
+      result = op.nil? ? "" : "<#{@op}> "
+      result += "transaction"
+      result += " <#{@name}>" if @name
+      result
     end
   end
 end
--- sentry-ruby/spec/sentry/transaction_spec.rb
+++ sentry-ruby/spec/sentry/transaction_spec.rb
@@ -25,6 +25,143 @@ RSpec.describe Sentry::Transaction do
     end
   end
 
+  describe "#set_initial_sample_desicion" do
+    before do
+      Sentry.init do |config|
+        config.dsn = DUMMY_DSN
+        config.transport.transport_class = Sentry::DummyTransport
+      end
+    end
+
+    context "when tracing is not enabled" do
+      before do
+        allow(Sentry.configuration).to receive(:tracing_enabled?).and_return(false)
+      end
+
+      it "sets @sampled to false and return" do
+        allow(Sentry.configuration).to receive(:tracing_enabled?).and_return(false)
+
+        transaction = described_class.new(sampled: true)
+        transaction.set_initial_sample_desicion
+        expect(transaction.sampled).to eq(false)
+      end
+    end
+
+    context "when tracing is enabled" do
+      let(:subject) { described_class.new(op: "rack.request", parent_sampled: true) }
+
+      before do
+        allow(Sentry.configuration).to receive(:tracing_enabled?).and_return(true)
+      end
+
+      context "when the transaction already has a decision" do
+        it "doesn't change it" do
+          transaction = described_class.new(sampled: true)
+          transaction.set_initial_sample_desicion
+          expect(transaction.sampled).to eq(true)
+
+          transaction = described_class.new(sampled: false)
+          transaction.set_initial_sample_desicion
+          expect(transaction.sampled).to eq(false)
+        end
+      end
+
+      context "when traces_sampler is not set" do
+        before do
+          Sentry.configuration.traces_sample_rate = 0.5
+        end
+
+        it "uses traces_sample_rate for sampling (positive result)" do
+          allow(Random).to receive(:rand).and_return(0.4)
+          expect(Sentry.configuration.logger).to receive(:debug).with(
+            "[Tracing] Starting <rack.request> transaction"
+          )
+
+          subject.set_initial_sample_desicion
+          expect(subject.sampled).to eq(true)
+        end
+
+        it "uses traces_sample_rate for sampling (negative result)" do
+          allow(Random).to receive(:rand).and_return(0.6)
+          expect(Sentry.configuration.logger).to receive(:debug).with(
+            "[Tracing] Discarding <rack.request> transaction because it's not included in the random sample (sampling rate = 0.5)"
+          )
+
+          subject.set_initial_sample_desicion
+          expect(subject.sampled).to eq(false)
+        end
+      end
+
+      context "when traces_sampler is provided" do
+        it "ignores the sampler if it's not callable" do
+          Sentry.configuration.traces_sampler = ""
+
+          expect do
+            subject.set_initial_sample_desicion
+          end.not_to raise_error
+        end
+
+        it "calls the sampler with sampling_context" do
+          sampling_context = {}
+
+          Sentry.configuration.traces_sampler = lambda do |context|
+            sampling_context = context
+          end
+
+          subject.set_initial_sample_desicion(foo: "bar")
+
+          # transaction_context's sampled attribute will be the old value
+          expect(sampling_context[:transaction_context].keys).to eq(subject.to_hash.keys)
+          expect(sampling_context[:parent_sampled]).to eq(true)
+          expect(sampling_context[:foo]).to eq("bar")
+        end
+
+        it "disgards the transaction if generated sample rate is not valid" do
+          expect(Sentry.configuration.logger).to receive(:warn).with(
+            "[Tracing] Discarding <rack.request> transaction because of invalid sample_rate: foo"
+          )
+
+          Sentry.configuration.traces_sampler = -> (_) { "foo" }
+          subject.set_initial_sample_desicion
+
+          expect(subject.sampled).to eq(false)
+        end
+
+        it "uses the genereted rate for sampling (positive)" do
+          expect(Sentry.configuration.logger).to receive(:debug).with(
+            "[Tracing] Starting transaction"
+          ).exactly(2)
+
+          subject = described_class.new
+          Sentry.configuration.traces_sampler = -> (_) { true }
+          subject.set_initial_sample_desicion
+          expect(subject.sampled).to eq(true)
+
+          subject = described_class.new
+          Sentry.configuration.traces_sampler = -> (_) { 1.0 }
+          subject.set_initial_sample_desicion
+          expect(subject.sampled).to eq(true)
+        end
+
+        it "uses the genereted rate for sampling (negative)" do
+          expect(Sentry.configuration.logger).to receive(:debug).with(
+            "[Tracing] Discarding transaction because traces_sampler returned 0 or false"
+          ).exactly(2)
+
+          subject = described_class.new
+          Sentry.configuration.traces_sampler = -> (_) { false }
+          subject.set_initial_sample_desicion
+          expect(subject.sampled).to eq(false)
+
+          subject = described_class.new
+          Sentry.configuration.traces_sampler = -> (_) { 0.0 }
+          subject.set_initial_sample_desicion
+          expect(subject.sampled).to eq(false)
+        end
+      end
+    end
+  end
+
   describe "#to_hash" do
     it "returns correct data" do
       hash = subject.to_hash
