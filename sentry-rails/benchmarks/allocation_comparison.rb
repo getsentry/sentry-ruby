0e51766a2876239c9482f8f120b04f62e161d36f
st0012 <stan001212@gmail.com>
2020-11-27 17:48:26 +0800

    Add allocation comparison script to sentry-rails

---
+++ sentry-rails/benchmarks/allocation_comparison.rb
@@ -0,0 +1,29 @@
+require 'benchmark/memory'
+require "sentry-ruby"
+require "sentry/benchmarks/benchmark_transport"
+require_relative "application"
+
+TestApp.configure do |config|
+  config.middleware.delete ActionDispatch::DebugExceptions
+  config.middleware.delete ActionDispatch::ShowExceptions
+end
+
+app = create_app do |config|
+  config.logger = ::Logger.new(STDOUT)
+  config.transport.transport_class = Sentry::BenchmarkTransport
+  config.breadcrumbs_logger = [:active_support_logger]
+end
+
+app.get("/exception")
+
+Benchmark.memory do |x|
+  x.report("master") { app.get("/exception") }
+  x.report("branch") { app.get("/exception") }
+
+  x.compare!
+  x.hold!("/tmp/allocation_comparison.json")
+end
+
+# transport = Sentry.get_current_client.transport
+# puts(transport.events.count)
+# puts(transport.events.first)
