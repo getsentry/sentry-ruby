460ff6219972dc6469a80013e85b456078b7355b
st0012 <stan001212@gmail.com>
2020-12-16 18:05:49 +0800

    Refactor Sentry's Railtie
    1. Give every patch/extension their own method to clarify their
       purposes.
    2. Move most of the patches into after_initializer hook from their own
       initializers. This is for making the order of each step more
       understandable.

--- sentry-rails/lib/sentry/rails/railtie.rb
+++ sentry-rails/lib/sentry/rails/railtie.rb
@@ -8,31 +8,36 @@ require "sentry/rails/overrides/streaming_reporter"
 
 module Sentry
   class Railtie < ::Rails::Railtie
+    # middlewares can't be injected after initialize
     initializer "sentry.use_rack_middleware" do |app|
       app.config.middleware.insert 0, Sentry::Rails::CaptureExceptions
     end
 
-    initializer 'sentry.action_controller' do
+    config.after_initialize do
+      configure_sentry_logger
+      extend_controller_methods
+      extend_active_job
+      override_exceptions_handling
+      override_streaming_reporter
+      setup_backtrace_cleanup_callback
+      inject_breadcrumbs_logger
+      activate_tracing
+    end
+
+    def configure_sentry_logger
+      Sentry.configuration.logger = ::Rails.logger
+    end
+
+    def extend_active_job
+      ActiveJob::Base.send(:prepend, Sentry::Rails::ActiveJobExtensions)
+    end
+
+    def extend_controller_methods
       ActiveSupport.on_load :action_controller do
         include Sentry::Rails::ControllerMethods
         include Sentry::Rails::ControllerTransaction
         ActionController::Live.send(:prepend, Sentry::Rails::Overrides::StreamingReporter)
       end
-    end
-
-    initializer 'sentry.action_view' do
-      ActiveSupport.on_load :action_view do
-        ActionView::StreamingTemplateRenderer::Body.send(:prepend, Sentry::Rails::Overrides::StreamingReporter)
-      end
-    end
-
-    config.after_initialize do
-      Sentry.configuration.logger = ::Rails.logger
-
-      setup_backtrace_cleanup_callback
-      inject_breadcrumbs_logger
-      override_exceptions_handling
-      activate_tracing
     end
 
     def inject_breadcrumbs_logger
@@ -63,16 +68,16 @@ module Sentry
       end
     end
 
+    def override_streaming_reporter
+      ActiveSupport.on_load :action_view do
+        ActionView::StreamingTemplateRenderer::Body.send(:prepend, Sentry::Rails::Overrides::StreamingReporter)
+      end
+    end
+
     def activate_tracing
       if Sentry.configuration.tracing_enabled?
         Sentry::Rails::Tracing.subscribe_tracing_events
         Sentry::Rails::Tracing.patch_active_support_notifications
-      end
-    end
-
-    initializer 'sentry.active_job' do
-      ActiveSupport.on_load :active_job do
-        require 'sentry/rails/active_job'
       end
     end
   end
