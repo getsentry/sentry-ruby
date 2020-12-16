107c2e3fa9c19415fbae6834f7f8e97bdbc8eecd
st0012 <stan001212@gmail.com>
2020-12-16 17:09:06 +0800

    Add rake example

---
+++ sentry-ruby/examples/rake/Rakefile.rb
@@ -0,0 +1,9 @@
+require "sentry-ruby"
+
+Sentry.init do |config|
+  config.dsn = 'https://2fb45f003d054a7ea47feb45898f7649@o447951.ingest.sentry.io/5434472'
+end
+
+task :raise_exception do
+  1/0
+end
