b66c5cf764a012c3d7c45fa32f33cd9015e74a2c
st0012 <stan001212@gmail.com>
2020-12-18 18:16:09 +0800

    Update example

--- sentry-ruby/examples/without_integrations/app.rb
+++ sentry-ruby/examples/without_integrations/app.rb
@@ -4,4 +4,4 @@ Sentry.init do |config|
   config.dsn = 'https://2fb45f003d054a7ea47feb45898f7649@o447951.ingest.sentry.io/5434472'
 end
 
-Sentry.capture_message("test Sentry")
+Sentry.capture_message("test Sentry", hint: { background: false })
