5d281a1a1633ccf665d92ae20c65fca891652fc8
st0012 <stan001212@gmail.com>
2021-01-15 16:39:10 +0800

    Document methods of the Sentry module

--- sentry-ruby/lib/sentry-ruby.rb
+++ sentry-ruby/lib/sentry-ruby.rb
@@ -41,10 +41,12 @@ module Sentry
   end
 
   class << self
+    # Returns a hash that contains all the integrations that have been registered to the main SDK.
     def integrations
       @integrations ||= {}
     end
 
+    # Registers the SDK integration with its name and version.
     def register_integration(name, version)
       meta = { name: "sentry.ruby.#{name}", version: version }.freeze
       integrations[name.to_s] = meta
@@ -70,26 +72,19 @@ module Sentry
       @background_worker = Sentry::BackgroundWorker.new(config)
     end
 
-    def initialized?
-      !!@main_hub
-    end
-
+    # Returns the main thread's active hub.
     def get_main_hub
       @main_hub
     end
 
-    def logger
-      configuration.logger
+    # Takes an instance of Sentry::Breadcrumb and stores it to the current active scope.
+    def add_breadcrumb(breadcrumb)
+      get_current_scope.breadcrumbs.record(breadcrumb)
     end
 
-    def add_breadcrumb(breadcrumb, &block)
-      get_current_scope.breadcrumbs.record(breadcrumb, &block)
-    end
-
-    def get_current_client
-      get_current_hub&.current_client
-    end
-
+    # Returns the current active hub.
+    # If the current thread doesn't have an active hub, it will clone the main thread's active hub,
+    # stores it in the current thread, and then returns it.
     def get_current_hub
       # we need to assign a hub to the current thread if it doesn't have one yet
       #
@@ -99,38 +94,79 @@ module Sentry
       Thread.current[THREAD_LOCAL] || clone_hub_to_current_thread
     end
 
-    def clone_hub_to_current_thread
-      Thread.current[THREAD_LOCAL] = get_main_hub.clone
+    # Returns the current active client.
+    def get_current_client
+      get_current_hub&.current_client
     end
 
+    # Returns the current active scope.
     def get_current_scope
       get_current_hub&.current_scope
     end
 
-    def with_scope(&block)
-      get_current_hub&.with_scope(&block)
+    # Clones the main thread's active hub and stores it to the current thread.
+    def clone_hub_to_current_thread
+      Thread.current[THREAD_LOCAL] = get_main_hub.clone
     end
 
+    # Takes a block and yields the current active scope.
+    #
+    # ```ruby
+    # Sentry.configure_scope do |scope|
+    #   scope.set_tags(foo: "bar")
+    # end
+    #
+    # Sentry.capture_message("test message") # this event will have tags { foo: "bar" }
+    # ```
+    #
     def configure_scope(&block)
       get_current_hub&.configure_scope(&block)
     end
 
-    def capture_event(event)
-      get_current_hub&.capture_event(event)
+    # Takes a block and yields a temporary scope.
+    # The temporary scope will inherit all the attributes from the current active scope and replace it to be the active
+    # scope inside the block. For example:
+    #
+    # ```ruby
+    # Sentry.configure_scope do |scope|
+    #   scope.set_tags(foo: "bar")
+    # end
+    #
+    # Sentry.capture_message("test message") # this event will have tags { foo: "bar" }
+    #
+    # Sentry.with_scope do |temp_scope|
+    #   temp_scope.set_tags(foo: "baz")
+    #   Sentry.capture_message("test message 2") # this event will have tags { foo: "baz" }
+    # end
+    #
+    # Sentry.capture_message("test message 3") # this event will have tags { foo: "bar" }
+    # ```
+    #
+    def with_scope(&block)
+      get_current_hub&.with_scope(&block)
     end
 
+    # Takes an exception and reports it to Sentry via the currently active hub.
     def capture_exception(exception, **options, &block)
       get_current_hub&.capture_exception(exception, **options, &block)
     end
 
+    # Takes a message string and reports it to Sentry via the currently active hub.
     def capture_message(message, **options, &block)
       get_current_hub&.capture_message(message, **options, &block)
     end
 
+    # Takes an instance of Sentry::Event and dispatches it to the currently active hub.
+    def capture_event(event)
+      get_current_hub&.capture_event(event)
+    end
+
+    # Takes or initializes a new Sentry::Transaction and makes a sampling decision for it.
     def start_transaction(**options)
       get_current_hub&.start_transaction(**options)
     end
 
+    # Returns the id of the lastly reported Sentry::Event.
     def last_event_id
       get_current_hub&.last_event_id
     end
@@ -140,6 +176,14 @@ module Sentry
       return if result.nil? || result.empty? || ($CHILD_STATUS && $CHILD_STATUS.exitstatus != 0)
 
       result.strip
+    end
+
+    def initialized?
+      !!@main_hub
+    end
+
+    def logger
+      configuration.logger
     end
   end
 end
