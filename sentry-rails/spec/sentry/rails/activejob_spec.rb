38b38913e006f3f63db8cd80ded2906d6d198aab
Stan Lo <stan001212@gmail.com>
2021-01-20 10:07:42 +0800

    Continue ActiveJob execution if Sentry is not initialized (#1217)
    Sentry.with_scope will swallow the block when the SDK is not
    initialized. So when users initialize the SDK conditionally, sometimes
    the job won't be executed at all.
    
    This fixes #1216 and #1211

--- sentry-rails/spec/sentry/rails/activejob_spec.rb
+++ sentry-rails/spec/sentry/rails/activejob_spec.rb
@@ -1,14 +1,20 @@
 require "spec_helper"
 
-class MyActiveJob < ActiveJob::Base
+class FailedJob < ActiveJob::Base
   self.logger = nil
 
   class TestError < RuntimeError
   end
 
   def perform
-    Sentry.get_current_scope.set_extras(foo: :bar)
     raise TestError, "Boom!"
+  end
+end
+
+class MyActiveJob < FailedJob
+  def perform
+    Sentry.get_current_scope.set_extras(foo: :bar)
+    super
   end
 end
 
@@ -16,6 +22,18 @@ class RescuedActiveJob < MyActiveJob
   rescue_from TestError, :with => :rescue_callback
 
   def rescue_callback(error); end
+end
+
+RSpec.describe "without Sentry initialized" do
+  before(:each) do
+    FailedJob.queue_adapter = :inline
+  end
+
+  it "runs job" do
+    job = FailedJob.new
+
+    expect { job.perform_now }.to raise_error(FailedJob::TestError)
+  end
 end
 
 RSpec.describe "ActiveJob integration" do
