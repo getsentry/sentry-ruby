99e4376cb28132c94f090ef701701c2bd5123b23
Stan Lo <stan001212@gmail.com>
2021-01-21 18:56:21 +0800

    Copy request env before Rails' ShowExceptions middleware (#1223)
    * Update examples app for running in production mode
    
    * Copy request env before Rails' ShowExceptions middleware
    
    When Rails' `ShowExceptions` middleware calls `#render_exception`, it
    also updates some of the env's headers, like changing `PATH_INFO` to
    `/500`.
    
    And if the current scope holds the same env hash as ShowExceptions does, this
    behavior will affect the reported event as well. Because
    the CaptureExceptions middleware will always run after ShowExceptions
    is called.
    
    So the solution to copy current scope's rack env when
    we know the exceptions app is going to be rendered.
    
    Notes:
    - sentry-raven patches ShowExceptions and reports events before
      `ShowExceptions#render_exception` is called, so it doesn't have such issue.
    - `ShowExceptions#render_exception` only changes first-level env
      attributes. So a simple `dup` should be able to solve the issue, don't
      need to deep-copy the entire env hash.
    
    * Update changelog

--- sentry-rails/lib/sentry/rails/rescued_exception_interceptor.rb
+++ sentry-rails/lib/sentry/rails/rescued_exception_interceptor.rb
@@ -11,6 +11,15 @@ module Sentry
         begin
           @app.call(env)
         rescue => e
+          request = ActionDispatch::Request.new(env)
+
+          # Rails' ShowExceptions#render_exception will mutate env for the exceptions app
+          # so we need to hold a copy of env to report the accurate data (like request's url)
+          if request.show_exceptions?
+            scope = Sentry.get_current_scope
+            scope.set_rack_env(scope.rack_env.dup)
+          end
+
           env["sentry.rescued_exception"] = e if Sentry.configuration.rails.report_rescued_exceptions
           raise e
         end
