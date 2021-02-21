08425555e6feabb75f41c71e91c5bbea10a80945
st0012 <stan001212@gmail.com>
2021-02-21 15:13:32 +0800

    Update example app's config

--- sentry-rails/examples/rails-6.0/config/initializers/sentry.rb
+++ sentry-rails/examples/rails-6.0/config/initializers/sentry.rb
@@ -4,7 +4,9 @@ Sentry.init do |config|
   config.traces_sample_rate = 1.0 # set a float between 0.0 and 1.0 to enable performance monitoring
   config.dsn = 'https://2fb45f003d054a7ea47feb45898f7649@o447951.ingest.sentry.io/5434472'
   config.release = `git branch --show-current`
-  config.async = lambda do |event, hint|
-    Sentry::SendEventJob.perform_later(event, hint)
-  end
+  # you can use the pre-defined job for the async callback
+  #
+  # config.async = lambda do |event, hint|
+  #   Sentry::SendEventJob.perform_later(event, hint)
+  # end
 end
