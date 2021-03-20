aba52a741b6e1b59c9c6b20bbc44f059e3917fde
st0012 <stan001212@gmail.com>
2021-03-20 23:07:16 +0800

    Add sentry/webpack and sourcemap related configs

--- sentry-rails/examples/rails-6.0/config/webpack/development.js
+++ sentry-rails/examples/rails-6.0/config/webpack/development.js
@@ -2,4 +2,19 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+const SentryCliPlugin = require("@sentry/webpack-plugin")
+
+environment.config.merge({ devtool: "source-map" })
+environment.plugins.prepend(
+  "Sentry",
+  new SentryCliPlugin({
+    include: "./public/",
+    ignore: ["node_modules"],
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+    org: "sentry-sdks",
+    project: "sentry-ruby",    
+    stripPrefix: ["public"],
+  })
+)
+
 module.exports = environment.toWebpackConfig()
