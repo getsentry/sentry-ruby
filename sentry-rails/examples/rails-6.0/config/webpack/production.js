3cf82a792511818afa80187fd7b39db2f8dd942b
st0012 <stan001212@gmail.com>
2021-03-20 23:38:17 +0800

    Setup production example

--- sentry-rails/examples/rails-6.0/config/webpack/production.js
+++ sentry-rails/examples/rails-6.0/config/webpack/production.js
@@ -2,4 +2,20 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+const SentryCliPlugin = require("@sentry/webpack-plugin")
+
+environment.config.merge({ devtool: "source-map" })
+environment.plugins.prepend(
+  "Sentry",
+  new SentryCliPlugin({
+    include: "./public/",
+    ignore: ["node_modules"],
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+    org: "sentry-sdks",
+    project: "sentry-ruby",    
+    stripPrefix: ["public"],
+  })
+)
+
 module.exports = environment.toWebpackConfig()
+
