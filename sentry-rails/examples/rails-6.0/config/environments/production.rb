4c186bb1b1479779d68fcc2aac709e09797fcfed
st0012 <stan001212@gmail.com>
2021-03-21 23:11:09 +0800

    WIP

--- sentry-rails/examples/rails-6.0/config/environments/production.rb
+++ sentry-rails/examples/rails-6.0/config/environments/production.rb
@@ -25,6 +25,7 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
+  config.assets.prefix = "public"
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
