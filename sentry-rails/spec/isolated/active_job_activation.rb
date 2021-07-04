eaec6b05160f45c91bb6f661858425697d7e776e
st0012 <stan001212@gmail.com>
2021-07-04 12:52:50 +0800

    Add isolated test case for ActiveJob extension

---
+++ sentry-rails/spec/isolated/active_job_activation.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+# for https://github.com/getsentry/sentry-ruby/issues/1249
+require "active_job"
+require "sentry/rails"
+require "minitest/autorun"
+
+class TestApp < Rails::Application
+end
+
+IO_STUB = StringIO.new
+
+app = TestApp
+
+# to simulate jobs being load during the eager_load initializer
+app.initializer :eager_load! do
+  Object.class_eval <<~CODE
+    class ApplicationJob < ActiveJob::Base
+      self.logger = Logger.new(IO_STUB)
+      self.queue_adapter = :inline
+
+      rescue_from Exception do |exception|
+        logger.info(">>> RESCUED")
+      end
+    end
+
+    class ErrorJob < ApplicationJob
+      around_perform do |job, block|
+        result = block.call
+        logger.info(">>> I SHOULD NEVER BE EXECUTED!")
+      end
+
+      def perform
+        1/0
+      end
+    end
+  CODE
+end
+
+app.config.eager_load = true
+app.initializer :sentry do
+  Sentry.init do |config|
+    config.logger = Logger.new(nil)
+    config.dsn = 'https://2fb45f003d054a7ea47feb45898f7649@o447951.ingest.sentry.io/5434472'
+    config.background_worker_threads = 0
+  end
+end
+
+app.initialize!
+
+class ActiveJobExtensionsTest < ActiveSupport::TestCase
+  def test_the_extension_is_loaded_before_eager_load_is_called
+    ErrorJob.perform_later
+
+    log_result = IO_STUB.string
+    assert_match(/RESCUED/, log_result, "ApplicationJob's rescue_from should be called")
+    refute_match(/I SHOULD NEVER BE EXECUTED/, log_result, "ErrorJob's around_perform should not be triggered")
+  end
+end
