77ce990802c431030e6db2f894c499c63523244d
st0012 <stan001212@gmail.com>
2021-10-02 12:48:39 +0800

    Enable ActiveStorage in Rails 6 example app

--- sentry-rails/examples/rails-6.0/app/controllers/posts_controller.rb
+++ sentry-rails/examples/rails-6.0/app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content, :cover)
     end
 end
