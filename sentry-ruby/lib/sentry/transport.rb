9fd99a870da5eb0a82b1d674f1b5ee0d274f3c23
Neel Shah <neel.shah@sentry.io>
2021-11-04 14:18:28 +0100

    Append client report to existing envelope max once a minute

--- sentry-ruby/lib/sentry/transport.rb
+++ sentry-ruby/lib/sentry/transport.rb
@@ -5,6 +5,7 @@ module Sentry
   class Transport
     PROTOCOL_VERSION = '7'
     USER_AGENT = "sentry-ruby/#{Sentry::VERSION}"
+    CLIENT_REPORT_INTERVAL = 60
 
     include LoggingHelper
 
@@ -17,7 +18,11 @@ module Sentry
       @transport_configuration = configuration.transport
       @dsn = configuration.dsn
       @rate_limits = {}
-      @discarded_events = Hash.new(0)
+
+      if configuration.send_client_reports
+        @discarded_events = Hash.new(0)
+        @last_client_report_sent = Time.now
+      end
     end
 
     def send_data(data, options = {})
@@ -87,15 +92,25 @@ module Sentry
 
     def encode(event)
       # Convert to hash
-      event_hash = event.to_hash
+      event_payload = event.to_hash
 
-      event_id = event_hash[:event_id] || event_hash["event_id"]
-      item_type = get_item_type(event_hash)
+      event_id = event_payload[:event_id] || event_payload["event_id"]
+      item_type = get_item_type(event_payload)
+
+      envelope_header = {
+        event_id: event_id,
+        dsn: @dsn.to_s,
+        sdk: Sentry.sdk_meta,
+        sent_at: Sentry.utc_now.iso8601
+      }
+
+      event_header = { type: item_type, content_type: 'application/json' }
 
       envelope = <<~ENVELOPE
-        {"event_id":"#{event_id}","dsn":"#{@dsn.to_s}","sdk":#{Sentry.sdk_meta.to_json},"sent_at":"#{Sentry.utc_now.iso8601}"}
-        {"type":"#{item_type}","content_type":"application/json"}
-        #{JSON.generate(event_hash)}
+        #{JSON.generate(envelope_header)}
+        #{JSON.generate(event_header)}
+        #{JSON.generate(event_payload)}
+        #{fetch_pending_client_report}
       ENVELOPE
 
       log_info("Sending envelope [#{item_type}] #{event_id} to Sentry")
@@ -123,6 +138,38 @@ module Sentry
 
     def get_item_type(event_hash)
       event_hash[:type] || event_hash["type"] || "event"
+    end
+
+    def fetch_pending_client_report
+      return nil unless configuration.send_client_reports
+      return nil if @last_client_report_sent > Time.now - CLIENT_REPORT_INTERVAL
+      return nil if @discarded_events.empty?
+
+      discarded_events_hash = @discarded_events.map do |key, val|
+        reason, type = key
+
+        # 'event' has to be mapped to 'error'
+        category = type == 'transaction' ? 'transaction' : 'error'
+
+        { reason: reason, category: category, quantity: val }
+      end
+
+      item_header = { type: 'client_report' }
+
+      item_payload = {
+        timestamp: Sentry.utc_now.iso8601,
+        discarded_events: discarded_events_hash
+      }
+
+      client_report_item = <<~CLIENT_REPORT_ITEM
+        #{JSON.generate(item_header)}
+        #{JSON.generate(item_payload)}
+      CLIENT_REPORT_ITEM
+
+      @discarded_events = Hash.new(0)
+      @last_client_report_sent = Time.now
+
+      client_report_item
     end
   end
 end
