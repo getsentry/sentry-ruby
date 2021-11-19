9eeb0b47fd6f392e19d41bb603f58064c1b0ddb5
st0012 <stan001212@gmail.com>
2021-11-19 12:16:56 +0000

    Refactor Scope setters

--- sentry-ruby/lib/sentry/scope.rb
+++ sentry-ruby/lib/sentry/scope.rb
@@ -112,7 +112,7 @@ module Sentry
     end
 
     def set_extra(key, value)
-      @extra.merge!(key => value)
+      set_extras(key => value)
     end
 
     def set_tags(tags_hash)
@@ -121,7 +121,7 @@ module Sentry
     end
 
     def set_tag(key, value)
-      @tags.merge!(key => value)
+      set_tags(key => value)
     end
 
     def set_contexts(contexts_hash)
@@ -131,7 +131,7 @@ module Sentry
 
     def set_context(key, value)
       check_argument_type!(value, Hash)
-      @contexts.merge!(key => value)
+      set_contexts(key => value)
     end
 
     def set_level(level)
