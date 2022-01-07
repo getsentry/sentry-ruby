9c0fa6e9d772cb6458a4e55c9bbb107509efce2d
st0012 <stan001212@gmail.com>
2022-01-07 15:26:34 +0000

    Remove obsolete Interface registration logic

--- sentry-ruby/lib/sentry/interface.rb
+++ sentry-ruby/lib/sentry/interface.rb
@@ -2,16 +2,6 @@
 
 module Sentry
   class Interface
-    def self.inherited(klass)
-      name = klass.name.split("::").last.downcase.gsub("interface", "")
-      registered[name.to_sym] = klass
-      super
-    end
-
-    def self.registered
-      @@registered ||= {} # rubocop:disable Style/ClassVars
-    end
-
     def to_hash
       Hash[instance_variables.map { |name| [name[1..-1].to_sym, instance_variable_get(name)] }]
     end
