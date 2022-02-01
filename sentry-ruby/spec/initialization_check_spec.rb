e3a3d0a1bc1a91e3d923bb19002d8c3e1bbbffce
st0012 <stan001212@gmail.com>
2022-02-01 16:24:46 +0000

    Don't cause NoMethodError for certain Sentry.* calls when the SDK is not initialized

---
+++ sentry-ruby/spec/initialization_check_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+
+RSpec.describe "with uninitialized SDK" do
+  before do
+    # completely nuke any initialized hubs
+    Sentry.instance_variable_set(:@main_hub, nil)
+    expect(Sentry.initialized?).to eq(false)
+  end
+
+  it { expect(Sentry.configuration).to eq(nil) }
+  it { expect(Sentry.send_event(nil)).to eq(nil) }
+  it { expect(Sentry.capture_exception(Exception.new)).to eq(nil) }
+  it { expect(Sentry.capture_message("foo")).to eq(nil) }
+  it { expect(Sentry.capture_event(nil)).to eq(nil) }
+  it { expect(Sentry.set_tags(foo: "bar")).to eq(nil) }
+  it { expect(Sentry.set_user(name: "John")).to eq(nil) }
+  it { expect(Sentry.set_extras(foo: "bar")).to eq(nil) }
+  it { expect(Sentry.set_context(foo:  { bar: "baz" })).to eq(nil) }
+  it { expect(Sentry.last_event_id).to eq(nil) }
+  it do
+    expect { Sentry.configure_scope { raise "foo" } }.not_to raise_error(RuntimeError)
+  end
+
+  it do
+    expect { Sentry.with_scope { raise "foo" } }.not_to raise_error(RuntimeError)
+  end
+end
+
