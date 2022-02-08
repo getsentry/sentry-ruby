ecc7b2ef44feab905b0189180e31200bfedb56a4
Neel Shah <neel.shah@sentry.io>
2022-02-08 14:24:43 +0100

    Start implementing Session class WIP

---
+++ sentry-ruby/lib/sentry/session.rb
@@ -0,0 +1,36 @@
+require 'securerandom'
+
+module Sentry
+  class Session
+    # TODO-neel docs, types
+    attr_accessor :sid
+    attr_accessor :did
+    attr_accessor :timestamp
+    attr_accessor :started
+    attr_accessor :duration
+    attr_accessor :status
+    attr_accessor :release
+    attr_accessor :environment
+    attr_accessor :user_agent
+    attr_accessor :ip_address
+    attr_accessor :errors
+    attr_accessor :user
+    attr_accessor :mode
+
+    def initialize(mode: :application, **options)
+      @mode = mode
+      @sid = options[:sid] || SecureRandom.uuid.delete('-')
+      @started = options[:started] || Sentry.utc_now
+      @status = options[:status] || :ok
+
+      update(options)
+    end
+
+    def update(**options)
+    end
+
+    def deep_dup
+      dup
+    end
+  end
+end
