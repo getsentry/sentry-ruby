e8e8bf72c8585d325fc6776e77d014e6cb1354a1
Neel Shah <neelshah.sa@gmail.com>
2022-02-21 13:19:40 +0100

    ref(envelopes): Add send_envelope and move rate limiting filtering to items level

--- sentry-ruby/lib/sentry/envelope.rb
+++ sentry-ruby/lib/sentry/envelope.rb
@@ -3,24 +3,47 @@
 module Sentry
   # @api private
   class Envelope
-    def initialize(headers)
+    class Item
+      attr_accessor :headers, :payload
+
+      def initialize(headers, payload)
+        @headers = headers
+        @payload = payload
+      end
+
+      def type
+        @headers[:type] || 'event'
+      end
+
+      def to_s
+        <<~ITEM
+          #{JSON.generate(@headers)}
+          #{JSON.generate(@payload)}
+        ITEM
+      end
+    end
+
+    attr_accessor :headers, :items
+
+    def initialize(headers = {})
       @headers = headers
       @items = []
     end
 
     def add_item(headers, payload)
-      @items << [headers, payload]
+      @items << Item.new(headers, payload)
     end
 
     def to_s
-      payload = @items.map do |item_headers, item_payload|
-        <<~ENVELOPE
-          #{JSON.generate(item_headers)}
-          #{JSON.generate(item_payload)}
-        ENVELOPE
-      end.join("\n")
+      [JSON.generate(@headers), *@items.map(&:to_s)].join("\n")
+    end
 
-      "#{JSON.generate(@headers)}\n#{payload}"
+    def item_types
+      @items.map(&:type)
+    end
+
+    def event_id
+      @headers[:event_id]
     end
   end
 end
