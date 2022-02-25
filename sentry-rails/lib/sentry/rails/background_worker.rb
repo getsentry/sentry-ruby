9bcea082a955d8a199d4a0b7a316446923385505
st0012 <stan001212@gmail.com>
2022-02-25 11:48:11 +0000

    Replace manual connection control with abstracted execution wrapper
    As described in https://github.com/rails/rails/blob/1d9d53831600c974fcccd4b5574f0e03d7e1513c/guides/source/threading_and_code_execution.md#executor
    
    `Rails.application.executor.wrap` should provide a higher-level of
    abstraction to achieve concurrent execution, which includes returning
    acquired ActiveRecord connection.
    
    This prevents us from locking in a particular ORM's database connection
    mechanism.

--- sentry-rails/lib/sentry/rails/background_worker.rb
+++ sentry-rails/lib/sentry/rails/background_worker.rb
@@ -2,7 +2,7 @@ module Sentry
   class BackgroundWorker
     def _perform(&block)
       # make sure the background worker returns AR connection if it accidentally acquire one during serialization
-      ActiveRecord::Base.connection_pool.with_connection do
+      ::Rails.application.executor.wrap do
         block.call
       end
     end
