a8035621b96b5f01ebe0918dcf23544b61df0f75
st0012 <stan001212@gmail.com>
2022-03-17 22:23:29 +0000

    Ignore internal Sidekiq::JobRetry::Skip exception

--- sentry-sidekiq/lib/sentry/sidekiq/configuration.rb
+++ sentry-sidekiq/lib/sentry/sidekiq/configuration.rb
@@ -4,10 +4,13 @@ module Sentry
 
     add_post_initialization_callback do
       @sidekiq = Sentry::Sidekiq::Configuration.new
+      @excluded_exceptions = @excluded_exceptions.concat(Sentry::Sidekiq::IGNORE_DEFAULT)
     end
   end
 
   module Sidekiq
+    IGNORE_DEFAULT = ["Sidekiq::JobRetry::Skip"]
+
     class Configuration
       # Set this option to true if you want Sentry to only capture the last job
       # retry if it fails.
