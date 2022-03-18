ff4878867d2d796f2d147986c7c3dec10be8800b
Stan Lo <stan001212@gmail.com>
2022-03-18 11:37:38 +0000

    Ignore internal Sidekiq::JobRetry::Skip exception (#1763)

--- sentry-sidekiq/spec/sentry/sidekiq/configuration_spec.rb
+++ sentry-sidekiq/spec/sentry/sidekiq/configuration_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe Sentry::Sidekiq::Configuration do
     expect(config.sidekiq).to be_a(described_class)
   end
 
+  it "adds Sidekiq::JobRetry::Skip to the ignore list" do
+    config = Sentry::Configuration.new
+
+    expect(config.excluded_exceptions).to include("Sidekiq::JobRetry::Skip")
+  end
+
   describe "#report_after_job_retries" do
     it "has correct default value" do
       expect(subject.report_after_job_retries).to eq(false)
