c53d406105770ddb9d97718c0e240e48e6ffc5c3
Stan Lo <stan001212@gmail.com>
2022-06-20 17:05:18 +0800

    Refactor ErrorSubscriber
    Co-authored-by: Jean byroot Boussier <jean.boussier+github@shopify.com>

--- sentry-rails/lib/sentry/rails/error_subscriber.rb
+++ sentry-rails/lib/sentry/rails/error_subscriber.rb
@@ -3,13 +3,13 @@ module Sentry
     # This is not a user-facing class. You should use it with Rails 7.0's error reporter feature and its interfaces.
     # See https://github.com/rails/rails/blob/main/activesupport/lib/active_support/error_reporter.rb for more information.
     class ErrorSubscriber
-      SKIP_SOURCES = [/.*_cache_store.active_support/]
+      SKIP_SOURCES = Regexp.union([/.*_cache_store.active_support/])
 
       def report(error, handled:, severity:, context:, source: nil)
         tags = { handled: handled }
 
         if source
-          return if SKIP_SOURCES.any? { |skip_source| skip_source.match?(source) }
+          return if SKIP_SOURCES.match?(source)
           tags[:source] = source
         end
 
