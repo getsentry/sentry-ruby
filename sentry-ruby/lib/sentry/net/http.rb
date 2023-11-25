f35f153afc75837e2495bc6c475ee9b2ae8c1535
Natik Gadzhi <natik@respawn.io>
2023-11-25 10:59:43 -0800

    require resolv in http patch, not in http_transport

--- sentry-ruby/lib/sentry/net/http.rb
+++ sentry-ruby/lib/sentry/net/http.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "net/http"
+require "resolv"
 
 module Sentry
   # @api private
--- sentry-ruby/lib/sentry/transport/http_transport.rb
+++ sentry-ruby/lib/sentry/transport/http_transport.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "net/http"
-require "resolv"
 require "zlib"
 
 module Sentry
