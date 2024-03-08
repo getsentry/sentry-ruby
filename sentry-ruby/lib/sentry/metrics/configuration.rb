da14eafe630064b89c4fa845d32cd349c60580ee
Neel Shah <neelshah.sa@gmail.com>
2024-03-08 15:04:53 +0100

    Add config.metrics.before_emit callback

--- sentry-ruby/lib/sentry/metrics/configuration.rb
+++ sentry-ruby/lib/sentry/metrics/configuration.rb
@@ -3,14 +3,37 @@
 module Sentry
   module Metrics
     class Configuration
+      include ArgumentCheckingHelper
+
       # Enable metrics usage
       # Starts a new {Sentry::Metrics::Aggregator} instance to aggregate metrics
       # and a thread to aggregate flush every 5 seconds.
       # @return [Boolean]
       attr_accessor :enabled
 
+      # Optional Proc, called before emitting a metric to the aggregator.
+      # Use it to filter keys (return false/nil) or update tags.
+      # Make sure to return true at the end.
+      #
+      # @example
+      #   config.metrics.before_emit = lambda do |key, tags|
+      #     return nil if key == 'foo'
+      #     tags[:bar] = 42
+      #     tags.delete(:baz)
+      #     true
+      #   end
+      #
+      # @return [Proc, nil]
+      attr_reader :before_emit
+
       def initialize
         @enabled = false
+      end
+
+      def before_emit=(value)
+        check_callable!("metrics.before_emit", value)
+
+        @before_emit = value
       end
     end
   end
