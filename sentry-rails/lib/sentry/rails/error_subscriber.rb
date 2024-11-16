b31f0f395af16953287e7154b383919c55d7a927
Peter Solnica <peter@solnica.online>
2024-11-16 23:03:38 +0100

    [rails] support string errors in error reporter (#2464)
    * [rails] support string errors in error reporter
    
    * Update CHANGELOG
    
    * [rails] log instead of raising when ErrorSubscriber fails
    
    * [rails] fix spec

--- sentry-rails/lib/sentry/rails/error_subscriber.rb
+++ sentry-rails/lib/sentry/rails/error_subscriber.rb
@@ -27,7 +27,20 @@ module Sentry
           hint.merge!(context.delete(:hint))
         end
 
-        Sentry::Rails.capture_exception(error, level: severity, contexts: { "rails.error" => context }, tags: tags, hint: hint)
+        options = { level: severity, contexts: { "rails.error" => context }, tags: tags, hint: hint }
+
+        case error
+        when String
+          Sentry::Rails.capture_message(error, **options)
+        when Exception
+          Sentry::Rails.capture_exception(error, **options)
+        else
+          log_debug("Expected an Exception or a String, got: #{error.inspect}")
+        end
+      end
+
+      def log_debug(message)
+        Sentry.configuration.logger.debug(message)
       end
     end
   end
