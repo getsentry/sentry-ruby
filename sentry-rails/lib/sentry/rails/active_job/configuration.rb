2f590905a95eee46071d435673f517df2472bb04
jonathan schatz <jon@tread.io>
2024-12-18 08:26:52 -0800

    add report_after_job_retries support for activejob

---
+++ sentry-rails/lib/sentry/rails/active_job/configuration.rb
@@ -0,0 +1,23 @@
+module Sentry
+  class Configuration
+    attr_reader :active_job
+
+    add_post_initialization_callback do
+      @active_job = Sentry::Rails::ActiveJob::Configuration.new
+    end
+  end
+
+  module Rails
+    module ActiveJob
+      class Configuration
+        # Set this option to true if you want Sentry to only capture the last job
+        # retry if it fails.
+        attr_accessor :report_after_job_retries
+
+        def initialize
+          @report_after_job_retries = false
+        end
+      end
+    end
+  end
+end
