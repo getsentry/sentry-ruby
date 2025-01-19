b0d2f3b487f6950c73cace43d149c5c9326532bb
Stan Lo <stan001212@gmail.com>
2025-01-19 13:17:58 +0000

    Use SolidQueue as the job enqueuer for development

--- sentry-rails/examples/rails-8.0/config/environments/development.rb
+++ sentry-rails/examples/rails-8.0/config/environments/development.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = { database: { writing: :queue } }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Make code changes take effect immediately without server restart.
