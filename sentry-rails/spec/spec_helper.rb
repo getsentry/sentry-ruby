ed1321e2a30503bca382e8c789da9fae2ae3ee44
Peter Solnica <peter@solnica.online>
2025-03-06 13:10:45 +0000

    Properly detach retry_stopped.active_job subscriber
    Otherwise specs would be failing randomly

--- sentry-rails/spec/spec_helper.rb
+++ sentry-rails/spec/spec_helper.rb
@@ -52,6 +52,10 @@ RSpec.configure do |config|
     expect(Sentry::Rails::Tracing.subscribed_tracing_events).to be_empty
     Sentry::Rails::Tracing.remove_active_support_notifications_patch
 
+    if defined?(Sentry::Rails::ActiveJobExtensions::SentryReporter)
+      Sentry::Rails::ActiveJobExtensions::SentryReporter.detach_retry_stopped_subscriber
+    end
+
     reset_sentry_globals!
   end
 
