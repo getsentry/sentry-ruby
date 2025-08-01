46bdcd5a1942f25402689f59922dde44e5c3569d
Peter Solnica <peter@solnica.online>
2025-08-01 16:14:40 +0200

    WIP

--- .devcontainer/entrypoint-sentry-test.sh
+++ .devcontainer/entrypoint-sentry-test.sh
@@ -4,11 +4,12 @@ set -e
 echo "Setting up sentry-test environment..."
 cd /workspace/sentry
 
+sudo chown -R sentry:sentry /workspace/sentry
+git config --global --add safe.directory /workspace/sentry
+
 # Only bundle install in the root folder for e2e test execution
 echo "Installing bundle dependencies in root folder..."
 bundle install
-
-sudo chown -R sentry:sentry /workspace/sentry
 
 echo "✅ sentry-test setup completed!"
 
