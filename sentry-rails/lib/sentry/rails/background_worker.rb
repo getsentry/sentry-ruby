b8bf85506e5b8eef2078dff01664748830b6d76a
Yaroslav <iaroslav2k@gmail.com>
2025-09-04 15:56:09 +0400

    Silence `_perform` method redefinition warning (#2682)
    I'd like to get rid of this noisy warning:
    
    ```shell
    sentry-rails-5.26.0/lib/sentry/rails/background_worker.rb:5: warning: method redefined; discarding old _perform
    sentry-ruby-5.26.0/lib/sentry/background_worker.rb:74: warning: previous definition of _perform was here
    ```
    
    #skip-changelog
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    #
    # Date:      Sun Aug 24 01:04:29 2025 +0400
    #
    # On branch silence-perform-method-redefinition-warning
    # Changes to be committed:
    #	modified:   lib/sentry/rails/background_worker.rb
    #

--- sentry-rails/lib/sentry/rails/background_worker.rb
+++ sentry-rails/lib/sentry/rails/background_worker.rb
@@ -2,14 +2,18 @@
 
 module Sentry
   class BackgroundWorker
-    def _perform(&block)
-      block.call
-    ensure
-      # some applications have partial or even no AR connection
-      if ActiveRecord::Base.connected?
-        # make sure the background worker returns AR connection if it accidentally acquire one during serialization
-        ActiveRecord::Base.connection_pool.release_connection
+    module ActiveRecordConnectionPatch
+      def _perform(&block)
+        super(&block)
+      ensure
+        # some applications have partial or even no AR connection
+        if ActiveRecord::Base.connected?
+          # make sure the background worker returns AR connection if it accidentally acquire one during serialization
+          ActiveRecord::Base.connection_pool.release_connection
+        end
       end
     end
   end
 end
+
+Sentry::BackgroundWorker.prepend(Sentry::BackgroundWorker::ActiveRecordConnectionPatch)
