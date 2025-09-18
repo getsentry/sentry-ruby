ebe526823a5cb3db539d46b4edfac32c4c7adcb2
Neel Shah <neel.shah@sentry.io>
2025-09-18 11:59:09 +0200

    Archive sentry-raven (#2708)
    Moved to https://github.com/getsentry/raven-ruby

--- sentry-raven/.scripts/bump-version.rb
+++
@@ -1,5 +0,0 @@
-file_name = "lib/raven/version.rb"
-
-text = File.read(file_name)
-new_contents = text.gsub(/VERSION = ".*"/, "VERSION = \"#{ARGV[1]}\"")
-File.open(file_name, "w") {|file| file.puts new_contents }
