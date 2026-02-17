833931b4a1237f57c839f38063773430d4264bc3
Neel Shah <neel.shah@sentry.io>
2026-02-17 13:27:50 +0100

    Update sentry-opentelemetry/lib/sentry/opentelemetry/otlp_propagator.rb
    Co-authored-by: Peter Solnica <peter@solnica.online>

--- sentry-opentelemetry/lib/sentry/opentelemetry/otlp_propagator.rb
+++ sentry-opentelemetry/lib/sentry/opentelemetry/otlp_propagator.rb
@@ -11,15 +11,20 @@ module Sentry
         span_context = ::OpenTelemetry::Trace.current_span(context).context
         return unless span_context.valid?
 
-        sampled = span_context.trace_flags.sampled? ? "1" : "0"
-        sentry_trace = "#{span_context.hex_trace_id}-#{span_context.hex_span_id}-#{sampled}"
-        setter.set(carrier, SENTRY_TRACE_HEADER_NAME, sentry_trace)
+        setter.set(carrier, SENTRY_TRACE_HEADER_NAME, to_sentry_trace(span_context))
 
         baggage = context[SENTRY_BAGGAGE_KEY]
         if baggage.is_a?(Sentry::Baggage)
           baggage_string = baggage.serialize
-          setter.set(carrier, BAGGAGE_HEADER_NAME, baggage_string) if baggage_string && !baggage_string.empty?
+          setter.set(carrier, BAGGAGE_HEADER_NAME, baggage_string) unless baggage_string&.empty?
         end
+      end
+
+      private
+
+      def to_sentry_trace(span_context)
+        sampled = span_context.trace_flags.sampled? ? "1" : "0"
+        "#{span_context.hex_trace_id}-#{span_context.hex_span_id}-#{sampled}"
       end
     end
   end
