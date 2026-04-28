d8fe9350adf90ee622c3b23b2afb9904479413b8
Peter Solnica <peter@solnica.online>
2026-04-28 08:06:11 +0000

    test: add shared examples for ActiveJob discard semantics

--- sentry-rails/spec/active_job/shared_examples/sentry_instrumented_backend.rb
+++ sentry-rails/spec/active_job/shared_examples/sentry_instrumented_backend.rb
@@ -3,4 +3,5 @@
 RSpec.shared_examples "a Sentry-instrumented ActiveJob backend" do
   it_behaves_like "an ActiveJob backend that captures errors"
   it_behaves_like "an ActiveJob backend that respects retry semantics"
+  it_behaves_like "an ActiveJob backend that respects discard semantics"
 end
