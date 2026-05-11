d6cd2e661e48ec2ac388c05bccc16325f511d554
Peter Solnica <peter@solnica.online>
2026-05-11 11:53:46 +0200

    fix(e2e): use playwright-managed chromium

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,7 +25,7 @@ Capybara.register_driver :selenium_headless_chrome do |app|
   options.add_argument("--no-sandbox")
   options.add_argument("--disable-gpu")
   options.add_argument("--temp-profile")
-  options.binary = "/usr/bin/chromium" if File.exist?("/usr/bin/chromium")
+  options.binary = "/home/sentry/.local/bin/chromium" if File.exist?("/home/sentry/.local/bin/chromium")
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
