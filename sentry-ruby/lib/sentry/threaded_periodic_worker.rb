a34cdba86d59c36a3bc12e811103c2e6a332af47
Neel Shah <neel.shah@sentry.io>
2026-09-14 16:42:02 +0200

    fix(metrics, logs): Add a ConditionVariable to TelemetryEventBuffer so that both periodic and size based flushing happen on the buffer thread

--- sentry-ruby/lib/sentry/threaded_periodic_worker.rb
+++ sentry-ruby/lib/sentry/threaded_periodic_worker.rb
@@ -4,41 +4,84 @@ module Sentry
   class ThreadedPeriodicWorker
     include LoggingHelper
 
+    attr_reader :thread
+
     def initialize(sdk_logger, interval)
       @thread = nil
       @exited = false
       @interval = interval
       @sdk_logger = sdk_logger
+
+      @thread_mutex = Mutex.new
+      @wake_condition = ConditionVariable.new
+      @idle_condition = ConditionVariable.new
+
+      @force_run = false
+      @running = false
     end
 
     def ensure_thread
-      return false if @exited
-      return true if @thread&.alive?
+      @thread_mutex.synchronize do
+        return false if @exited
+        return true if @thread&.alive?
 
-      @thread = Thread.new do
-        loop do
-          sleep(@interval)
-          run
+        @thread = Thread.new do
+          loop do
+            @thread_mutex.synchronize do
+              @wake_condition.wait(@thread_mutex, @interval) unless @force_run
+              @force_run = false
+              @running = true
+            end
+
+            begin
+              run
+            ensure
+              @thread_mutex.synchronize do
+                @running = false
+                @idle_condition.broadcast
+              end
+            end
+          end
         end
-      end
 
-      true
+        true
+      end
     rescue ThreadError
+      @thread_mutex.synchronize { @exited = true }
       log_debug("[#{self.class.name}] thread creation failed")
-      @exited = true
       false
     end
 
+    def wake
+      @thread_mutex.synchronize do
+        @force_run = true
+        @wake_condition.signal
+      end
+
+      true
+    end
+
+    def wait_until_idle
+      @thread_mutex.synchronize do
+        @idle_condition.wait(@thread_mutex) while @running || @force_run
+      end
+    end
+
     def kill
-      @exited = true
+      thread = @thread_mutex.synchronize do
+        @exited = true
+        @force_run = false
+        @idle_condition.broadcast
+        @thread
+      end
 
       # Only a started worker has a thread to kill (and to log about).
       # Guarding here keeps a never-started worker's teardown silent, so
       # killing one during test reset can't emit a stray debug line.
-      return unless @thread
+      return unless thread
 
       log_debug("[#{self.class.name}] thread killed")
-      @thread.kill
+      thread.kill
     end
   end
 end
--- sentry-ruby/spec/support/shared_examples_for_telemetry_event_buffers.rb
+++ sentry-ruby/spec/support/shared_examples_for_telemetry_event_buffers.rb
@@ -49,11 +49,16 @@ RSpec.shared_examples "telemetry event buffer" do |event_factory:, max_items_con
       2.times { subject.add_item(event) }
     end
 
-    it "auto-flushes pending items to the client when the number of items reaches max_items" do
-      expect(client).to receive(:send_envelope)
+    it "auto-flushes pending items to the client using the buffer thread when the number of items reaches max_items" do
+      thread = nil
+      expect(client).to receive(:send_envelope) do
+        thread = Thread.current
+      end
 
       3.times { subject.add_item(event) }
 
+      subject.flush
+      expect(thread).to eq(subject.thread)
       expect(subject).to be_empty
     end
   end
@@ -95,7 +100,7 @@ RSpec.shared_examples "telemetry event buffer" do |event_factory:, max_items_con
     let(:max_items) { 30 }
 
     it "thread-safely handles concurrent access" do
-      expect(client).to receive(:send_envelope).exactly(3).times
+      expect(client).to receive(:send_envelope).at_least(:once)
 
       threads = 3.times.map do
         Thread.new do
@@ -169,6 +174,7 @@ RSpec.shared_examples "telemetry event buffer" do |event_factory:, max_items_con
         3.times { subject.add_item(event) }
       }.not_to raise_error
 
+      subject.flush
       expect(reentrant_calls).to be >= 1
     end
 
@@ -182,6 +188,7 @@ RSpec.shared_examples "telemetry event buffer" do |event_factory:, max_items_con
 
       3.times { subject.add_item(event) }
 
+      subject.flush
       expect(items_sent).not_to be_empty
       expect(string_io.string).not_to include("deadlock")
     end
@@ -214,12 +221,14 @@ RSpec.shared_examples "telemetry event buffer" do |event_factory:, max_items_con
       it "logs the error to sdk_logger" do
         3.times { subject.add_item(event) }
 
+        subject.flush
         expect(string_io.string).to include("Failed to send #{event.class}")
       end
 
       it "clears the buffer after a failed send to avoid memory buildup" do
         3.times { subject.add_item(event) }
 
+        subject.flush
         expect(subject).to be_empty
       end
     end
